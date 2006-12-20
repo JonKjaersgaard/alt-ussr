7904c99a3cefb3db0010417dea837da95cce6733
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2006-12-20 11:34:04 +0000

    Added a comment
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@150 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/comm/Receiver.java
+++ src/ussr/comm/Receiver.java
@@ -2,6 +2,12 @@ package ussr.comm;
 
 public interface Receiver {
 
+    /**
+     * Test whether this receiver is compatible with (can receive) the type of transmission
+     * indicated by the argument.
+     * @param type the type of the transmission to compare with
+     * @return true if they are compatible, false otherwise
+     */
     boolean isCompatible(TransmissionType type);
 
     void receive(Packet data);
