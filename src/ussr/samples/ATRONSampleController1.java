c555a4938dba6865de12e80be957b59e3dd344b6
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2007-03-01 12:18:16 +0000

    Refactored ATRON sample controller into generic superclass with ATRON API operations and a specific sample controller
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@174 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/samples/ATRONSampleController1.java
+++ src/ussr/samples/ATRONSampleController1.java
@@ -7,12 +7,9 @@ package ussr.samples;
 
 import java.awt.Color;
 
-import com.sun.org.apache.bcel.internal.generic.GETSTATIC;
-
 import ussr.comm.Packet;
 import ussr.comm.Receiver;
 import ussr.comm.Transmitter;
-import ussr.model.ControllerImpl;
 
 /**
  * A simple controller for the ATRON, controlling connector stickiness based on
@@ -21,7 +18,7 @@ import ussr.model.ControllerImpl;
  * @author ups
  *
  */
-public class ATRONSampleController1 extends ControllerImpl {
+public class ATRONSampleController1 extends ATRONController {
 
 	private int[] roleOfConnector = {-1,-1,-1,-1,-1,-1,-1,-1};
     /**
@@ -143,87 +140,4 @@ public class ATRONSampleController1 extends ControllerImpl {
 			rotate(-1);
 		}
 	}
-
-	private boolean isRotating() {
-    	return module.getActuators().get(0).isActive();
-	}
-	private int getRotation() {
-    	float encVal = module.getActuators().get(0).getEncoderValue();
-    	if(Math.abs(encVal-0.50)<0.125f) return 0;
-    	if(Math.abs(encVal-0.75)<0.125f) return 1;
-    	if(Math.abs(encVal-0)<0.125f || Math.abs(encVal-1)<0.125f) return 2;
-    	if(Math.abs(encVal-0.25)<0.125f) return 3;
-    	System.err.println("No ATRON rotation defined should not happend "+encVal);
-    	return 0;
-    }
-    private void rotate(int dir) {
-    	float goalRot = 0;
-    	
-    	if(getRotation()==0) goalRot = ((dir>0)?1:3);
-    	else if(getRotation()==1) goalRot = ((dir>0)?2:0);
-    	else if(getRotation()==2) goalRot = ((dir>0)?3:1);
-    	else if(getRotation()==3) goalRot = ((dir>0)?0:2);
-    	
-//    	System.out.println("RotPos "+getRotation()+" go for "+goalRot/4f);
-    	module.getActuators().get(0).activate(goalRot/4f);
-    	while(isRotating()) {
-    		module.getActuators().get(0).activate(goalRot/4f);
-    		Thread.yield();
-    	}
-	}
-	private void disconnectAll() {
-		for(int i=0;i<8;i++) {
-			if(isConnected(i)) {
-				disconnect(i);
-			}
-		}
-	}
-	private void connectAll() {
-		for(int i=0;i<8;i++) {
-			if(isOtherConnectorNearby(i)) {
-				System.out.println(module.getID()+" Other connector at connector "+i);
-			}
-			if(canConnect(i)) {
-				connect(i);
-			}
-		}
-	}
-    
-	private boolean canConnect(int i) {
-		return isOtherConnectorNearby(i)&&isMale(i)&&!isConnected(i);
-	}
-	private boolean canDisconnect(int i) {
-		return isMale(i)&&isConnected(i);
-	}
-
-	private boolean isMale(int i) {
-		return i%2==0;
-	}
-
-	private void connect(int i) {
-		module.getConnectors().get(i).connect();
-	}
-	
-	private void disconnect(int i) {
-		module.getConnectors().get(i).disconnect();
-	}
-	private boolean isConnected(int i) {
-		return module.getConnectors().get(i).isConnected();
-	}
-
-    public void rotateContinuous(float dir) {
-    	module.getActuators().get(0).activate(dir);
-    }
-    
-    public boolean isOtherConnectorNearby(int connector) {
-    	if(module.getConnectors().get(connector).isConnected()) {
-    		return true;
-    	}
-    	if(module.getConnectors().get(connector).hasProximateConnector()) {
-    		return true;
-    	}
-    	else  {
-    		return false;
-    	}
-    }
 }
