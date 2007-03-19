5fda607085cb377e5f6fabeaab7affbab8ce919f
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2007-03-19 08:56:18 +0000

    Tilt sensors (partially hard-coded, still not generally useful)
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@200 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/samples/atron/ATRONController.java
+++ src/ussr/samples/atron/ATRONController.java
@@ -5,6 +5,7 @@ import ussr.comm.PacketReceivedObserver;
 import ussr.comm.Receiver;
 import ussr.model.ControllerImpl;
 import ussr.model.Module;
+import ussr.model.Sensor;
 
 public abstract class ATRONController extends ControllerImpl implements PacketReceivedObserver {
 
@@ -144,4 +145,19 @@ public abstract class ATRONController extends ControllerImpl implements PacketRe
     public void handleMessage(byte[] message, int messageSize, int channel) {
     	System.out.println("Message recieved please overwrite this method");
     }
+    private byte read(String name) {
+        for(Sensor sensor: module.getSensors()) {
+            if(name.equals(sensor.getName())) {
+                float value = sensor.readValue();
+                return (byte)(value/Math.PI*180-90); 
+            }
+                
+        }
+        throw new Error("Unknown sensor: "+name);
+    }
+    // Note: hard-coded to do forward-backward tilt sensoring for axles
+    public byte getTiltX() { return read("TiltSensor:y"); }
+    // Note: hard-coded to do side-to-side tilt sensoring for driver and axles
+    public byte getTiltY() { return read("TiltSensor:x"); }
+    public byte getTiltZ() { return read("TiltSensor:z"); }
 }
