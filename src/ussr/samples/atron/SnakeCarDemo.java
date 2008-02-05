e102d88a364688f99fe950995bf020b86154c251
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-02-05 20:01:10 +0000

    Complete demo: eight to snake, snake moves, snake to eight, eight to car, car drives (but car is often lying down when it starts to drive)
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@417 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ src/ussr/samples/atron/SnakeCarDemo.java
@@ -0,0 +1,154 @@
+package ussr.samples.atron;
+
+import ussr.model.Controller;
+import ussr.robotbuildingblocks.Robot;
+import ussr.robotbuildingblocks.WorldDescription;
+import ussr.samples.atron.EightToCarSimulationJ.EightController;
+
+public class SnakeCarDemo extends EightToCarSimulationJ {
+
+    public static void main(String argv[]) {
+        new SnakeCarDemo().main();
+    }
+
+    protected void changeWorldHook(WorldDescription world) {
+        // Override settings from superclas
+    }
+    @Override
+    protected Robot getRobot() {
+        return new ATRON() {
+            public Controller createController() {
+                return new CarStuffController();
+            }
+        };
+    }
+
+    public static final byte NO_CONNECTOR = 8;
+    public static final byte ALL_MODULES = 8;
+
+    protected class CarStuffController extends EightController {
+
+        volatile private int state = 0;
+        int snake_counter = 10;
+
+        @Override
+        public void activate_before_eight2car() {
+            int id = this.getMyID();
+            if(id==3) this.state = 1;
+            while(true) {
+                super.ussrYield();
+                if(state!=0) System.out.println("@ Pre-controller "+id+" in state "+state);
+                switch(state) {
+                case 1: /* module 3 */
+                    this.disconnect(0);
+                    this.disconnect(6);
+                    nextState(2,0);
+                    break;
+                case 2: /* module 0 */
+                    this.rotate(1);
+                    this.rotate(1);
+                    nextState(3,6);
+                    break; 
+                case 3: /* module 6 */
+                    this.rotate(1);
+                    this.rotate(1);
+                    nextState(9,2);
+                    //nextState(4,3);
+                    break;
+                case 4: /* module 3 */ /* NOT USED */
+                    for(int i=0; i<10; i++) {
+                        this.rotateDegrees(30);
+                        this.rotateDegrees(-30);
+                        delay(1000);
+                    }
+                    nextState(5,6);
+                    break;
+                case 5: /* module 6 */
+                    this.rotate(-1);
+                    this.rotate(-1);
+                    nextState(6,0);
+                    break;
+                case 6: /* module 0 */ 
+                    this.rotate(-1);
+                    this.rotate(-1);
+                    nextState(7,3);
+                    break; 
+                case 7: /* module 3 */
+                    this.connect(0);
+                    this.connect(6);
+                    nextState(8,ALL_MODULES);
+                    break;
+                case 8: /* all modules */
+                    super.delay(5000);
+                    return;
+                case 9: /* module 2 */
+                    //this.rotate(1);
+                    this.rotateContinuous(1);
+                    this.delay(6000);
+                    //this.centerStop();
+                    if(this.snake_counter-->0) {
+                        nextState(10,4);
+                    } else {
+                        this.centerStop();
+                        super.home();
+                        nextState(11,4);
+                    }
+                    break;
+                case 10: /* module 4 */
+                    //this.rotate(-1);
+                    this.rotateContinuous(-1);
+                    this.delay(6000);
+                    //this.centerStop();
+                    nextState(9,2);
+                    break;
+                case 11: /* module 4 */
+                    this.centerStop();
+                    super.home();
+                    nextState(5,6);
+                    break;
+                default:
+                    state = 0;
+                }
+            }
+        }
+
+        @Override
+        public void activate_after_eight2car() {
+            switch(this.getMyID()) {
+            case 5: this.rotateContinuous(0.1f); break;
+            case 2: this.rotateContinuous(-0.1f); break;
+            case 3: this.rotateContinuous(0.1f); break;
+            case 4: this.rotateContinuous(-0.1f); break;
+            }
+        }
+
+        private void nextState(int nextState, int module_id) {
+            this.state = 0;
+            this.sendNextState(nextState, NO_CONNECTOR, (byte)module_id, (byte)0);
+            if(module_id==ALL_MODULES) this.state = nextState;
+        }
+
+        private void sendNextState(int nextState, byte omit, byte destination, byte seen) {
+            seen |= 1<<this.getMyID();
+            byte[] message = new byte[] { 0, (byte)(nextState), 42, destination, seen };
+            for(byte c=0; c<8; c++)
+                if(c!=omit && super.isConnected(c)) super.sendMessage(message, (byte)4, c);
+        }
+
+        @Override
+        protected boolean canHandleMessage(byte[] incoming, int messageSize, int channel) {
+            if(incoming[2]==42) {
+                byte destination = incoming[3];
+                byte seen = incoming[4];
+                byte flag = (byte) (seen>>this.getMyID());
+                byte nextState = incoming[1];
+                if((flag&1)==1) return true;
+                if(destination==this.getMyID() || destination==ALL_MODULES) state = nextState;
+                if(destination!=this.getMyID() || destination==ALL_MODULES)
+                    sendNextState(nextState,(byte)channel,destination,seen);
+                return true;
+            }
+            return false;
+        }
+    }
+}
