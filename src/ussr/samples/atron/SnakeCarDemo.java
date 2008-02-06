d24b9d893b0c7f38044ee2e617a6372a9aca39c2
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-02-06 08:43:43 +0000

    Obstacles, appropriate behavior, works
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@419 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/samples/atron/SnakeCarDemo.java
+++ src/ussr/samples/atron/SnakeCarDemo.java
@@ -2,6 +2,7 @@ package ussr.samples.atron;
 
 import ussr.model.Controller;
 import ussr.robotbuildingblocks.Robot;
+import ussr.robotbuildingblocks.VectorDescription;
 import ussr.robotbuildingblocks.WorldDescription;
 import ussr.samples.atron.EightToCarSimulationJ.EightController;
 
@@ -11,8 +12,27 @@ public class SnakeCarDemo extends EightToCarSimulationJ {
         new SnakeCarDemo().main();
     }
 
+    private static final int N_CIRCLE_OBSTACLES = 16;
+    private static final float CIRCLE_OBSTACLE_RADIUS = 0.5f;
+    private static final float OBSTACLE_Y = -0.4f;
+    private static final float OBSTACLE_Y_INC = 0.15f;
     protected void changeWorldHook(WorldDescription world) {
-        // Override settings from superclas
+        // Override settings from superclass
+        System.out.println("Generating circle obstacles");
+        VectorDescription[] obstacles = new VectorDescription[N_CIRCLE_OBSTACLES*2];
+        int index = 0;
+        float y = OBSTACLE_Y;
+        for(int layers=0; layers<2; layers++) {
+            for(int i=0; i<N_CIRCLE_OBSTACLES; i++) {
+                obstacles[index++] = new VectorDescription(
+                        ((float)(CIRCLE_OBSTACLE_RADIUS*Math.cos(((double)i)/N_CIRCLE_OBSTACLES*Math.PI*2+y))),
+                        y,
+                        ((float)(CIRCLE_OBSTACLE_RADIUS*Math.sin(((double)i)/N_CIRCLE_OBSTACLES*Math.PI*2+y)))
+                );
+            }
+            y+=OBSTACLE_Y_INC;
+        }
+        world.setObstacles(obstacles);
     }
     @Override
     protected Robot getRobot() {
@@ -29,7 +49,7 @@ public class SnakeCarDemo extends EightToCarSimulationJ {
     protected class CarStuffController extends EightController {
 
         volatile private int state = 0;
-        int snake_counter = 1;// 10
+        int snake_counter = 5;
 
         @Override
         public void activate_before_eight2car() {
@@ -116,6 +136,7 @@ public class SnakeCarDemo extends EightToCarSimulationJ {
         public void activate_after_eight2car() {
             // First execute "getup" operation
             int id = this.getMyID();
+            if(id==0) reportTilt();
             if(id==0) 
                 this.state = 1;
             else
@@ -129,6 +150,7 @@ public class SnakeCarDemo extends EightToCarSimulationJ {
                     this.rotate(1);
                     this.rotate(1);
                     delay(20000);
+                    reportTilt();
                     nextState(2,6);
                     break;
                 case 2: /* module 6 */
@@ -170,9 +192,22 @@ public class SnakeCarDemo extends EightToCarSimulationJ {
             case 2: this.rotateContinuous(-1f); break;
             case 3: this.rotateContinuous(1f); break;
             case 4: this.rotateContinuous(-1f); break;
+            case 0:
+                int i;
+                while(true) {
+                    this.delay(1000);
+                    reportTilt();
+                }
             }
+            delay(500000);
+            this.centerStop();
+            while(true) this.ussrYield();
         }
 
+        private void reportTilt() {
+            System.out.println("["+this.getTiltX()+","+this.getTiltY()+","+this.getTiltZ()+"]");
+        }
+        
         private void nextState(int nextState, int module_id) {
             this.state = 0;
             this.sendNextState(nextState, NO_CONNECTOR, (byte)module_id, (byte)0);
