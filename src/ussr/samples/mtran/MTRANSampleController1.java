7abda92badbddaedc769105ed70361ddc4d04502
david <david@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-02-18 15:09:56 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@470 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/samples/mtran/MTRANSampleController1.java
+++ src/ussr/samples/mtran/MTRANSampleController1.java
@@ -7,8 +7,6 @@ package ussr.samples.mtran;
 
 import java.util.Random;
 
-import ussr.samples.GenericSimulation;
-
 /**
  * A simple controller for the ODIN robot, ossilates OdinMuscles with a random start state 
  * 
@@ -31,65 +29,22 @@ public class MTRANSampleController1 extends MTRANController {
     	while(module.getSimulation().isPaused()) ussrYield();
     	System.out.println("MTRAN RUNNING "+module.getID());
     	while(true) {
-            ussrYield();
-            if(!GenericSimulation.getActuatorsAreActive()) continue;
-    		double goal0 = Math.sin(4*module.getSimulation().getTime()+module.getID());
+    		double goal0 = Math.sin(3*module.getSimulation().getTime()+module.getID());
     		double cur0 =  2*(getEncoderPosition(0)-0.5);
-    		//System.out.println(goal+" "+cur);
     		if(cur0>goal0) {
     			rotate(-1, 0);
     		}else {
     			rotate(1, 0);
     		}
     		
-    		double goal1 = -Math.sin(4*module.getSimulation().getTime()+module.getID()+0.5f);
+    		double goal1 = -Math.sin(3*module.getSimulation().getTime()+module.getID()+0.5f);
     		double cur1 =  2*(getEncoderPosition(1)-0.5);
-    		//System.out.println(goal+" "+cur);
     		if(cur1>goal1) {
     			rotate(-1, 1);
     		}else {
     			rotate(1, 1);
     		}
-    		
-    		/*if(>0) {
-    			if(getAngularPositionDegrees(0)<90||getAngularPositionDegrees(0)>-90) {
-    				rotate(-1, 0);	
-    			}
-    			else {
-    				rotate(0, 0);
-    			}
-    		}
-    		else {
-    			//rotate(1, 0);
-    		}*/
-    		
-    	/*	rotate(-1, 0);rotate(-1, 1);delay(1000);
-    		rotate(-1, 0);rotate(1, 1);delay(1000);
-    		rotate(1, 0);rotate(1, 1);delay(1000);
-    		rotate(1, 0);rotate(-1, 1);delay(1000);*/
-    		/*rotateTo(0.4f, 1);
-    		rotateTo(0f, 1); delay(1000);
-    		rotateTo(0.4f, 1);
-    		rotateTo(0.6f, 1);
-    		rotateTo(1f, 1);delay(1000);
-    		rotateTo(0.6f, 1);
-    		rotateTo(0.5f, 1);delay(1000);
-    		
-    		rotateTo(0.4f, 0);
-    		rotateTo(0f, 0); delay(1000);
-    		rotateTo(0.4f, 0);
-    		rotateTo(0.6f, 0);
-    		rotateTo(1f, 0);delay(1000);
-    		rotateTo(0.6f, 0);
-    		rotateTo(0.5f, 0);delay(1000);
-    		
-    		rotateTo(1f, 0);
-    		rotateTo(1f, 1);
-    		rotateTo(0.6f, 1);
-    		rotateTo(0.4f, 1);
-    		rotateTo(0f, 1);
-    		
-    		delay(500000);*/
+    		ussrYield();
     	}
 	}
     
