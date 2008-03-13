ed9b3ee305469d8c3628b3ed6f71b5cd92fe14ff
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-03-13 12:54:21 +0000

    Moved DCD examples to Ulrik's home folder
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@543 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ home/ups/ussr/samples/atron/natives/dcd/ATRONDcdLongCarSimulation.java
@@ -0,0 +1,32 @@
+package ussr.samples.atron.natives.dcd;
+
+import java.util.ArrayList;
+
+import ussr.description.Robot;
+import ussr.description.geometry.VectorDescription;
+import ussr.description.setup.ModulePosition;
+import ussr.model.Controller;
+import ussr.physics.PhysicsParameters;
+import ussr.samples.ObstacleGenerator;
+import ussr.samples.atron.ATRON;
+import ussr.samples.atron.ATRONBuilder;
+
+public class ATRONDcdLongCarSimulation extends ATRONDCDCarSimulation {
+    
+    public static void main(String argv[]) {
+        ATRONDcdLongCarSimulation simulation = new ATRONDcdLongCarSimulation();
+        simulation.obstacleType = ObstacleGenerator.ObstacleType.CIRCLE;
+        simulation.main();
+    }
+
+    static VectorDescription pos(float x, float y, float z) {
+        final float Yoffset = 0.25f;
+        return new VectorDescription(x*ATRON.UNIT, y*ATRON.UNIT-Yoffset, z*ATRON.UNIT);
+    }
+    
+    @Override
+    protected ArrayList<ModulePosition> buildRobot() {
+        return new ATRONBuilder().buildCar(6,new VectorDescription(0,-0.25f,0));
+    }
+
+}
