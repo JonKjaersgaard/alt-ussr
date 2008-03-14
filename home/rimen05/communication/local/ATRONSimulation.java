1258f27b41d707aaa4f346980334a4271408b3e1
rimen05 <rimen05@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-03-14 14:24:39 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@556 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ home/rimen05/communication/local/ATRONSimulation.java
@@ -0,0 +1,67 @@
+/**
+ * Unified Simulator for Self-Reconfigurable Robots (USSR)
+ * (C) University of Southern Denmark 2008
+ * This software is distributed under the BSD open-source license.
+ * For licensing see the file LICENCE.txt included in the root of the USSR distribution.
+ */
+package ussr.samples.atron.simulations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import ussr.description.Robot;
+import ussr.description.geometry.RotationDescription;
+import ussr.description.geometry.VectorDescription;
+import ussr.description.setup.ModuleConnection;
+import ussr.description.setup.ModulePosition;
+import ussr.description.setup.WorldDescription;
+import ussr.model.Controller;
+import ussr.physics.PhysicsParameters;
+import ussr.samples.GenericSimulation;
+import ussr.samples.atron.ATRON;
+import ussr.samples.atron.ATRONBuilder;
+
+/**
+ * A sample ATRON simulation
+ * 
+ * @author Modular Robots @ MMMI
+ *
+ */
+public class ATRONSimulation1 extends GenericSimulation {
+	
+    public static void main( String[] args ) {
+        new ATRONSimulation1().main();
+    }
+
+    public void main() {
+        PhysicsParameters.get().setPhysicsSimulationStepSize(0.001f);
+        setConnectorsAreActive(true);
+        WorldDescription world = new WorldDescription();
+        world.setPlaneSize(5);
+        List<ModulePosition> modulePos;
+        ATRONBuilder builder = new ATRONBuilder(); 
+        //modulePos = buildAsLattice(5,2,4,1);
+        modulePos = builder.buildCar(4, new VectorDescription(0,-0.25f,0));
+       // modulePos = buildSnake(2);
+        // modulePos = Arrays.asList(new WorldDescription.ModulePosition[] { new WorldDescription.ModulePosition("hermit", new VectorDescription(2*0*unit,0*unit,0*unit), rotation_EW) });
+        world.setModulePositions(modulePos);
+        
+        ArrayList<ModuleConnection> connections = builder.allConnections();
+        world.setModuleConnections(connections);
+        
+        this.runSimulation(world,true);
+    }
+
+    @Override
+    protected Robot getRobot() {
+        return new ATRON() {
+            public Controller createController() {
+                return new ATRONSampleController1();
+            }
+        };
+    }
+
+ 
+
+}
--- home/rimen05/communication/local/OdinController.java
+++ home/rimen05/communication/local/OdinController.java
@@ -33,7 +33,7 @@ public class OdinController extends ussr.samples.odin.OdinController {
     static int ne = 0;
     static int nt = 0;
     //static int e = ((int)(1/pe));
-    static int e = (int)(pe*100);
+    static int e = (int)((pe*100)-1);
     static int id = -1;
     static boolean idDone = false;
     //static boolean peDone = false;
@@ -44,7 +44,7 @@ public class OdinController extends ussr.samples.odin.OdinController {
     int[] counters;
     int time = 0;
     List<Color> lastColors;
-    static float commInterval = 0.5f;
+    static float commInterval = 1.0f;
     static float blinkInterval = 0.5f*commInterval;
     //We can also access modules, which is a protected attribute of a parent class.
     
