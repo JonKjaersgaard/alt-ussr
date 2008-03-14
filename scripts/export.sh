9723e4a0186d2f77f1b06fc29b77700856c7b164
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-03-14 17:44:11 +0000

    Patch of .classpath file to remove e.g. home dir
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@567 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
 NAME=ussr-src-`head -1 VERSION.txt`.zip
+echo "Warning: temporarily renaming .classpath file"
+mv -f .classpath .classpath-export
+cat .classpath-export | grep -v home/ | grep -v jmf.jar > .classpath
 pushd ..
 rm -f ussr/${NAME}
-zip -r ussr/${NAME} ussr/README.txt ussr/HOWTO.txt ussr/INSTALL.pdf ussr/VERSION.txt ussr/bin/com ussr/bin/org ussr/bin/ussr ussr/doc ussr/resources ussr/src ussr/zoo -x '*.svn*'
+zip -r ussr/${NAME} ussr/{.classpath,.project} ussr/README.html ussr/doc ussr/LICENSE.txt ussr/VERSION.txt ussr/bin/{com,org,ussr} ussr/resources ussr/src ussr/zoo -x '*.svn*' -x '*~'
 popd
+mv -f .classpath-export .classpath
+echo "Restored .classpath"
