6fc2e35faa5afbd8462af908531acbae337b9fe7
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-03-14 19:00:49 +0000

    Last-minute fixes
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@570 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- scripts/export.sh
+++ scripts/export.sh
@@ -2,7 +2,7 @@
 NAME=ussr-src-`head -1 VERSION.txt`.zip
 echo "Warning: temporarily renaming .classpath file"
 mv -f .classpath .classpath-export
-cat .classpath-export | grep -v home/ | grep -v jmf.jar > .classpath
+cat .classpath-export | grep -v home/ | grep -v jmf.jar | grep -v sandbox > .classpath
 pushd ..
 rm -f ussr/${NAME}
 zip -r ussr/${NAME} ussr/{.classpath,.project} ussr/README.html ussr/doc ussr/LICENSE.txt ussr/VERSION.txt ussr/bin/{com,org,ussr} ussr/resources ussr/src ussr/zoo -x '*.svn*' -x '*~'
