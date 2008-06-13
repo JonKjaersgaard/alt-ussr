946ecce039851f89d0c49e53469d802ba8e91957
lamik06 <lamik06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-06-13 17:39:26 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@668 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ home/lamik06/sunTron/API1/ATRONControllerImpl.java
@@ -0,0 +1,26 @@
+package sunTron.API1;
+
+import ussr.model.Module;
+import ussr.samples.atron.ATRONController;
+
+public class ATRONControllerImpl extends ATRONController {
+
+//	public ATRONAPIImpl(Module module1) {
+//		this.setModule(module1);
+//		// TODO Auto-generated constructor stub
+//	}
+	SunTronAPIUSSR sunTronAPI;
+
+	public ATRONControllerImpl(SunTronAPIUSSR sunTronAPI) {
+		this.sunTronAPI = sunTronAPI;// TODO Auto-generated constructor stub
+	}
+	public void activate() {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void handleMessage(byte[] message, int messageSize, int channel) {
+		// TODO Auto-generated method stub
+		sunTronAPI.handleMessage(message, messageSize, channel);
+	}
+
+}
---
+++ home/lamik06/sunTron/API1/IControllerLoop.java
@@ -0,0 +1,12 @@
+package sunTron.API1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public interface IControllerLoop {
+
+	public void loop();
+	public boolean setDataFromDis(DataInputStream dis);
+	public DataOutputStream getDos(DataOutputStream dos);
+
+}
