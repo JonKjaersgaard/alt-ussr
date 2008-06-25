77970d539d369c16f0422d78cfb4d6aa5ddb82c0
rimen05 <rimen05@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-06-25 14:27:19 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@673 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- home/rimen05/benchmark.sh
+++ home/rimen05/benchmark.sh
@@ -2,9 +2,9 @@
 
 cd ../../
 n=1
-while [ $n -le 30 ]; do
+while [ $n -le 1 ]; do
   echo $n
-  make -f ./home/rimen05/makefile odin >> benchmark.txt
+  make -f ./home/rimen05/makefile atron >> benchmark.txt
   let n++
 done
 mv benchmark.txt ./home/rimen05/
