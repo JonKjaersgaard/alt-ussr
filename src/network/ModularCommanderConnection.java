d953ed9e5bd8918fd1d34f2dea67188a1da27e99
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-09-10 12:08:31 +0000

    renamed
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@708 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ src/network/ModularCommanderConnection.java
@@ -0,0 +1,61 @@
+package network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+import ussr.model.Controller;
+
+public class ModularCommanderConnection extends AbstractNetworkConnection {
+
+    private Mapper controller;
+    
+    public static interface Mapper {
+        public void mapPacketToAPI(byte[] packet);
+    }
+
+    public ModularCommanderConnection(int port, Mapper mapper) {
+        super(port);
+        this.controller = mapper;
+    }
+
+    @Override
+    public boolean activationHook(InputStream input, OutputStream output) {
+        System.out.println("Modular commander controller activated for "+controller);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String packet;
+        while(true) {
+            try {
+                System.out.println("Waiting to read line...");
+                packet = reader.readLine();
+                System.out.println("Read line...");
+            } catch (IOException e) {
+                throw new Error("Error reading from socket");
+            }
+            System.out.println("Read: "+packet);
+            if(packet==null) break;
+            byte[] bytes = ussrUnpackXML(packet);
+            controller.mapPacketToAPI(bytes);
+        }
+        System.out.println("Socket closed, waiting for new connection");
+        return true;
+    }
+
+    private byte[] ussrUnpackXML(String input) {
+        List<Byte> storage = new ArrayList<Byte>();
+        XMLTokenizer xe = new XMLTokenizer(input);
+        xe.eat("<modular_commander>");
+        while(xe.hasTag("b")) storage.add(xe.eatByte());
+        xe.eat("</modular_commander>");
+        byte[] result = new byte[storage.size()];
+        for(int i=0; i<result.length; i++) result[i] = storage.get(i);
+        return result;
+    }
+
+}
