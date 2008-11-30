2d0ce02736da59da0177e4764a1bdcc8fd1321d5
kokuz06 <kokuz06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2008-11-30 19:20:27 +0000

    Commit NR.1 from Quick prototyping project.  Here is committed InteractiveOdinBuilderTest1. java  the file to run first when working with Quick prototyping or interactive building of Odin robot
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@749 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ src/ussr/samples/odin/simulations/InteractiveOdinBuilderTest1.java
@@ -0,0 +1,92 @@
+/**
+ * Unified Simulator for Self-Reconfigurable Robots (USSR)
+ * (C) University of Southern Denmark 2008
+ * This software is distributed under the BSD open-source license.
+ * For licensing see the file LICENCE.txt included in the root of the USSR distribution.
+ */
+package ussr.samples.odin.simulations;
+
+import java.util.ArrayList;
+
+import ussr.description.Robot;
+import ussr.description.geometry.RotationDescription;
+import ussr.description.geometry.VectorDescription;
+import ussr.description.setup.ModuleConnection;
+import ussr.description.setup.ModulePosition;
+import ussr.description.setup.WorldDescription;
+import ussr.model.Controller;
+import ussr.physics.PhysicsFactory;
+import ussr.physics.PhysicsLogger;
+import ussr.physics.PhysicsParameters;
+import ussr.physics.PhysicsSimulation;
+import ussr.samples.GenericSimulation;
+import ussr.samples.odin.OdinBuilder;
+import ussr.samples.odin.modules.OdinBall;
+import ussr.samples.odin.modules.OdinMuscle;
+
+/**
+ * Simple Odin simulation test setup
+ * 
+ * @author david
+ *
+ */
+
+public class InteractiveOdinBuilderTest1 extends GenericSimulation {
+	
+    public static void main( String[] args ) {
+    	new InteractiveOdinBuilderTest1().runSimulation(null,true);
+    }
+    
+    public void runSimulation(WorldDescription world, boolean startPaused) {
+        PhysicsLogger.setDefaultLoggingLevel();
+        final PhysicsSimulation simulation = PhysicsFactory.createSimulator();
+        PhysicsParameters.get().setResolutionFactor(2); // Needed for large Odin structures 
+        
+        simulation.setRobot(new OdinMuscle(){
+        	public Controller createController() {
+        		return new OdinSampleController1("OdinMuscle");
+        	}},"OdinMuscle");
+        
+        simulation.setRobot(new OdinBall(){
+        	public Controller createController() {
+        		return new OdinSampleController1("OdinBall");
+        	}},"OdinBall");
+        
+        if(world==null) world = createWorld();
+        simulation.setWorld(world);
+        simulation.setPause(startPaused);
+
+        // Start
+        simulation.start();
+    }
+    
+    /**
+     * Create a world description for our simulation
+     * @return the world description
+     */
+    private static WorldDescription createWorld() {
+    	WorldDescription world = new WorldDescription();
+        world.setPlaneSize(5);
+        world.setHasBackgroundScenery(false);
+        world.setPlaneTexture(WorldDescription.WHITE_GRID_TEXTURE);
+        OdinBuilder builder = new OdinBuilder();
+        //int nBalls=0,xMax=0, yMax=0,zMax=0; = Arrays.asList(new WorldDescription.ModulePosition[] { new WorldDescription.ModulePosition("0","OdinMuscle", new VectorDescription(0,0,0), new RotationDescription(0,0,0))});
+        //int nBalls=2, xMax=1, yMax=2,zMax=2;
+       // int nBalls=3, xMax=3, yMax=2,zMax=2;
+       // int nBalls=4, xMax=3, yMax=2,zMax=2;
+       //int nBalls=8, xMax=3, yMax=2,zMax=2;
+       //int nBalls=14, xMax=3, yMax=3,zMax=3;
+        int nBalls=20, xMax=4, yMax=4,zMax=4;
+        //int nBalls=80, xMax=5, yMax=5,zMax=5; // Max on Ulrik's machine
+        ArrayList<ModulePosition> modulePos = builder.buildDenseBlob(nBalls,xMax,yMax,zMax);
+        world.setModulePositions(modulePos);
+        world.setModuleConnections(builder.allConnections());
+        System.out.println("#Total         = "+modulePos.size());
+        return world;
+    }
+
+	@Override
+	protected Robot getRobot() {
+		return Robot.NO_DEFAULT;
+	}
+}
