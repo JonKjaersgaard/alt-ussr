dcc30bd02d14a289187366441ab74c62cf29dc51
david <david@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-01-02 12:06:02 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@760 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ home/davidc/ASESocketTest/SocSnakeSimulation.java
@@ -0,0 +1,76 @@
+/**
+ * Unified Simulator for Self-Reconfigurable Robots (USSR)
+ * (C) University of Southern Denmark 2008
+ * This software is distributed under the BSD open-source license.
+ * For licensing see the file LICENCE.txt included in the root of the USSR distribution.
+ */
+package ASESocketTest;
+
+import java.util.ArrayList;
+
+import ussr.description.Robot;
+import ussr.description.geometry.VectorDescription;
+import ussr.description.setup.ModulePosition;
+import ussr.description.setup.WorldDescription;
+import ussr.model.Controller;
+import ussr.physics.PhysicsParameters;
+import ussr.samples.GenericSimulation;
+import ussr.samples.ObstacleGenerator;
+import ussr.samples.atron.ATRON;
+import ussr.samples.atron.GenericATRONSimulation;
+import ussr.samples.atron.network.ATRONReflectionController;
+import ussr.samples.atron.network.ATRONReflectionEventController;
+
+
+
+
+public class SocSnakeSimulation extends GenericATRONSimulation {
+	
+    private ObstacleGenerator.ObstacleType obstacle = ObstacleGenerator.ObstacleType.LINE;
+    
+	public static void main( String[] args ) {
+		PhysicsParameters.get().setWorldDampingLinearVelocity(0.5f);
+		PhysicsParameters.get().setRealisticCollision(true);
+		PhysicsParameters.get().setMaintainRotationalJointPositions(false);
+		new SocSnakeSimulation().main();
+    }
+	
+	protected Robot getRobot() {
+
+        ATRON robot = new ATRON() {
+            public Controller createController() {
+                return new ATRONReflectionEventController();
+            }
+        };
+        
+        robot.setGentle();
+        return robot;
+    }
+	
+	protected ArrayList<ModulePosition> buildSnake(int length) {
+    	float Yoffset = 0.4f;
+    	ArrayList<ModulePosition> mPos = new ArrayList<ModulePosition>();
+    	int x=0,y=0,z=0;
+    	int portRC = 9900;
+    	int portEvent = 9901;
+    	for(int i=0;i<length;i++) {
+    		if(i%2==0) {
+    			mPos.add(new ModulePosition(i+"", ";portRC="+portRC+";portEvent="+portEvent, new VectorDescription(x*ATRON.UNIT,y*ATRON.UNIT-Yoffset,z*ATRON.UNIT), ATRON.ROTATION_EW));
+    		}
+    		else {
+    			mPos.add(new ModulePosition(i+"", ";portRC="+portRC+";portEvent="+portEvent, new VectorDescription(x*ATRON.UNIT,y*ATRON.UNIT-Yoffset,z*ATRON.UNIT), ATRON.ROTATION_NS));
+    		}
+    		portRC+=2;portEvent+=2;
+    		x++;z++;
+    	}
+    	return mPos;
+	}
+
+	protected ArrayList<ModulePosition> buildRobot() {
+		return buildSnake(2);
+	}
+    
+    protected void changeWorldHook(WorldDescription world) {
+        startPaused = false;
+    }
+}
