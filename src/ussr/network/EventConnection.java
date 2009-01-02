e50036cdf54824cfc5fb64462b06eac2bbcb9ca3
david <david@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-01-02 12:08:49 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@766 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/network/EventConnection.java
+++ src/ussr/network/EventConnection.java
@@ -7,52 +7,82 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 public class EventConnection extends AbstractNetworkConnection {
 	BufferedReader reader;
 	BufferedWriter writer;
+	Socket connection;
 	int id = 1;
 	boolean debug = false; 
+	boolean reconnect = false;
 	public EventConnection(int port) {
 		super(port);
 	}
 
-	public boolean activationHook(InputStream input, OutputStream output) {
+	public boolean activationHook(InputStream input, OutputStream output, Socket connection) {
         reader = new BufferedReader(new InputStreamReader(input));
         writer = new BufferedWriter(new OutputStreamWriter(output));
-        return false;
+        this.connection = connection;
+        while(!reconnect) {
+        	try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+        }
+        try {
+
+        	reader.close();
+        	writer.close();
+			connection.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        reconnect = false;
+        return true;
 	}
-	
 	public String sendEvent(String eventType, Object[] eventParams) {
 		String packet = packEvent(eventType, eventParams);
 		if(debug) System.out.println("Packet: "+packet);
 		sendEvent(packet);
 		String reply = getReply();
-		if(debug) System.out.println("Reply: "+reply);
-		String[] parts = reply.split(" ");
-		int replyid = Integer.parseInt(parts[0]);
-		if(replyid==id||parts.length>2) {
-			if(parts[1].equals("OK")) {
-				id++;
-				return parts[2];
+		if(reply!=null) {
+			if(debug) System.out.println("Reply: "+reply);
+			String[] parts = reply.split(" ");
+			int replyid = Integer.parseInt(parts[0]);
+			if(replyid==id||parts.length>2) {
+				if(parts[1].equals("OK")) {
+					id++;
+					return parts[2];
+				}
+				else {
+					System.err.println("Warning: event error "+parts[1]+"!=OK");
+				}
 			}
 			else {
-				System.err.println("Warning: event error "+parts[1]+"!=OK");
+				System.err.println("Warning: illegal event packet received after sending "+packet);
 			}
 		}
 		else {
-			System.err.println("Warning: illegal packet received");
+			System.err.println("Warning: null event packet received after sending "+packet);
 		}
+		System.err.println("Event socket closed, reconnecting");
+		reconnect = true;
 		return null;
 	}
 	
 	private void sendEvent(String packet) {
+		if(writer==null) {
+			System.err.println("Event Connection not activated (no writer socket connected)");
+			return;
+		}
 		try {
             writer.write(packet);
             writer.newLine();
             writer.flush();
         } catch(IOException exn) {
-            throw new Error("Error writing to socket");
+            throw new Error("Error writing to event socket: "+packet);
         }
 		
 	}
@@ -71,6 +101,7 @@ public class EventConnection extends AbstractNetworkConnection {
 
 	private String paramToString(Object param) {
 		if(param.getClass().equals(Integer.class)) return param.toString();
+		else if(param.getClass().equals(Long.class)) return param.toString();
 		else if(param==Float.TYPE) return param.toString();
 		else if(param==Byte.TYPE) return param.toString();
 		else if(param==Character.TYPE) return param.toString();
@@ -87,10 +118,18 @@ public class EventConnection extends AbstractNetworkConnection {
 	    }
 	}
 	private String getReply() {
-	    try {
+		if(reader==null) {
+			System.err.println("Event Connection not activated (no reader socket connected)");
+			return "0 error";
+		}
+		try {
 	    	return reader.readLine(); 
         } catch (IOException e) {
             throw new Error("Error reading from socket");
         }
+	}
+
+	public boolean isReady() {
+		return !reconnect&&connection!=null&&!connection.isClosed();
 	}
 }
