d6a298f7f26f8a4fae5248ed9978d17cd1600cd7
david <david@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-02-06 12:59:36 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@804 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ src/ussr/util/learning/WifiCMBroadcaster.java
@@ -0,0 +1,56 @@
+package ussr.util.learning;
+
+import ussr.comm.Packet;
+import ussr.comm.RadioTransmitter;
+import ussr.comm.TransmissionType;
+import ussr.model.Module;
+import ussr.physics.PhysicsObserver;
+import ussr.physics.PhysicsSimulation;
+
+import com.jme.math.Vector3f;
+import com.sun.corba.se.impl.ior.ByteBuffer;
+
+public class WifiCMBroadcaster implements PhysicsObserver {
+	final byte LEARNING_MESSAGE = 4;
+	final byte label = 1;
+	PhysicsSimulation simulation;
+	double deltaT;
+	double nextT;
+	CMTracker tracker;
+	RadioTransmitter radio;
+	Vector3f oldPos;
+	byte stateCount;
+	public WifiCMBroadcaster(PhysicsSimulation simulation, double deltaT, CMTracker tracker) {
+		this.simulation = simulation;
+		this.deltaT = deltaT;
+		this.tracker = tracker;
+		Module dummyModule = new Module();
+		dummyModule.setSimulation(simulation);
+		radio = new RadioTransmitter(dummyModule, dummyModule,TransmissionType.RADIO, Float.MAX_VALUE);
+		nextT = simulation.getTime()+deltaT;
+		oldPos = tracker.getRobotCM();
+		stateCount = 0;
+	}
+	
+	/**
+	 * Transmit robots movement over wifi 
+	 */
+	public void physicsTimeStepHook(PhysicsSimulation simulation) {
+		if(nextT<simulation.getTime()) {
+			float dist = tracker.getRobotCM().distance(oldPos);
+			byte reward = (byte)(250*dist);
+			if(!Float.isNaN(dist)) {				
+	 			ByteBuffer bb = new ByteBuffer(3);
+				bb.append(LEARNING_MESSAGE);
+				bb.append(label);
+				bb.append(reward);
+				System.out.println("reward send = "+reward);
+				Packet packet = new Packet(bb.toArray());
+				radio.send(packet);
+			}
+			nextT += deltaT;
+			stateCount++;
+			oldPos = tracker.getRobotCM();
+		}
+	}
+}
