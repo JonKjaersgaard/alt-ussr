7de94e8d7c68e83c7e9dcf0878d6c87b59f7430a
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-08-05 20:40:18 +0000

    cleanup
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@976 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- native/dcd-deprecated/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DCD_CONFIG__
-#define __DCD_CONFIG__
-
-#include <stdint.h>
-
-#define DCD_OMIT_CACHE
-#define DCD_PERFECT_CACHE
-
-#endif
--- native/dcd-deprecated/runDcdTest.sh
+++
@@ -1,2 +0,0 @@
-gcc -I../include -g dcdTest.c navigation.c -o dcdTest.bin
-./dcdTest.bin
