0c1767bc0e366bb9b879749aaf39e4212fe740bf
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-08-05 14:49:58 +0000

    initial rev
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@965 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ src/ussr/samples/atron/natives/VMSimulation.java
@@ -0,0 +1,33 @@
+/**
+ * Unified Simulator for Self-Reconfigurable Robots (USSR)
+ * (C) University of Southern Denmark 2008
+ * This software is distributed under the BSD open-source license.
+ * For licensing see the file LICENCE.txt included in the root of the USSR distribution.
+ */
+package ussr.samples.atron.natives;
+
+import ussr.description.Robot;
+import ussr.model.Controller;
+import ussr.samples.atron.ATRON;
+import ussr.samples.atron.simulations.ATRONSnakeSimulation;
+
+/**
+ * Snake simulation using native controller
+ * 
+ * @author ups
+ *
+ */
+public class VMSimulation extends ATRONSnakeSimulation {
+	public static void main(String argv[]) {
+		new VMSimulation().main();
+	}
+
+	@Override
+	protected Robot getRobot() {
+		return new ATRON() {
+			public Controller createController() {
+				return new ATRONNativeController("vm");
+		}
+	};
+}
+}
