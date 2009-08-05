8fb39792c569e9ea818e263ae6e828bd8893e582
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-08-05 20:39:04 +0000

    added more files
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@974 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- scripts/export.sh
+++ scripts/export.sh
@@ -5,7 +5,7 @@ mv -f .classpath .classpath-export
 cat .classpath-export | grep -v home/ | grep -v jmf.jar | grep -v sandbox > .classpath
 pushd ..
 rm -f ussr/${NAME}
-zip -r ussr/${NAME} ussr/{.classpath,.project} ussr/README.html ussr/doc ussr/LICENSE.txt ussr/VERSION.txt ussr/bin/{com,org,ussr} ussr/resources ussr/src ussr/zoo -x '*.svn*' -x '*~'
+zip -r ussr/${NAME} ussr/{.classpath,.project} ussr/README.html ussr/README.txt ussr/doc ussr/LICENSE.txt ussr/VERSION.txt ussr/bin/{com,org,ussr} ussr/resources ussr/src ussr/zoo -x '*.svn*' -x '*~'
 popd
 mv -f .classpath-export .classpath
 echo "Restored .classpath"
