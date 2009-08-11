875360923bd298907c7fee7ac9df942f3863fe25
mirko <mirko@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-08-11 22:27:15 +0000

    added definitons of atomic start/end to atronvm.c
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@995 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- native/base/atronvm.c
+++ native/base/atronvm.c
@@ -5,6 +5,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 void JNICALL Java_ussr_samples_atron_natives_ATRONNativeController_nativeHandleMessage(JNIEnv *jniENV, jobject self, jint initializationContext, jbyteArray message, jint messageSize, jint channel) {
   unsigned char buffer[ATRON_MAX_MESSAGE_SIZE];
@@ -24,7 +25,25 @@ void VM_alarmStart(uint32_t moduleID, uint32_t dt) {
 extern void main();
 
 void activate(USSRONLY(USSREnv *env)) { 
-	main();
+  main_1();
 }
 
 int32_t initialize(USSRONLY(USSREnv *env)) { }
+
+
+typedef uint8_t __nesc_atomic_t;
+
+/* Saves current interrupt mask state and disables interrupts. */
+inline __nesc_atomic_t 
+__nesc_atomic_start(void) 
+{
+  //todo
+  return 0;
+}
+
+/* Restores interrupt mask to original state. */
+inline void 
+__nesc_atomic_end(__nesc_atomic_t original_SREG) 
+{
+  //todo
+}
