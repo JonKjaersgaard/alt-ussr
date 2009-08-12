38914f3cb19c556ccf3153d4d9249ef20d13a35a
mirko <mirko@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-08-12 03:49:08 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1001 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- native/include/ussr_atomic_functions.h
+++ native/include/ussr_atomic_functions.h
@@ -4,6 +4,8 @@
 #include "stdint.h"
 
 typedef uint8_t __nesc_atomic_t;
+inline void __nesc_enable_interrupt();
+inline void __nesc_disable_interrupt();
 __nesc_atomic_t __nesc_atomic_start(void);
 void __nesc_atomic_end(__nesc_atomic_t original_SREG);
 
