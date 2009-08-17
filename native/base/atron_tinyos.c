4eb9575d444ff41425934ce604cdb0630c801964
mirko <mirko@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-08-17 22:54:21 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1018 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- native/base/atron_tinyos.c
+++ native/base/atron_tinyos.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/*
 void JNICALL Java_ussr_samples_atron_natives_ATRONNativeController_nativeHandleMessage(JNIEnv *jniENV, jobject self, jint initializationContext, jbyteArray message, jint messageSize, jint channel) {
   unsigned char buffer[ATRON_MAX_MESSAGE_SIZE];
   USSREnv env;
@@ -16,6 +17,7 @@ void JNICALL Java_ussr_samples_atron_natives_ATRONNativeController_nativeHandleM
   ussr_byteArray2charArray(&env, message, messageSize, buffer);
   printf("handleMessage(&env, buffer, messageSize, channel);\n");
 }
+*/
 
 /* //   _AlarmUssrImplP__VM_alarmStart */
 /* void VM_alarmStart(uint32_t moduleID, uint32_t dt) { */
@@ -32,10 +34,3 @@ void activate(USSRONLY(USSREnv *env)) {
 
 int32_t initialize(USSRONLY(USSREnv *env)) { }
 
-/* // temporarily here to experiment */
-/* char sendMessage(USSREnv *env, unsigned char *message, unsigned char messageSize, unsigned char connector) { */
-/*   jbyteArray array = ussr_charArray2byteArray(env, message, messageSize); */
-/*   char result = ussr_call_byte_controller_method(env, "sendMessage", "([BBB)B", array, messageSize, connector); */
-/*   ussr_releaseByteArray(env, array); */
-/*   return result; */
-/* } */
