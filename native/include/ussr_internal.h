2ca7327763c1f16aeebdbd5186262b3d50400be6
mirko <mirko@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-08-21 18:23:28 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1030 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- native/include/ussr_internal.h
+++ native/include/ussr_internal.h
@@ -3,10 +3,12 @@
 
 #include <ussr.h>
 #include <jni.h>
+#include "stdint.h"
 
 extern void ussr_call_void_controller_method(USSREnv *env, char *name, char *signature, ...);
 extern int ussr_call_int_controller_method(USSREnv *env, char *name, char *signature, ...);
 extern int ussr_call_byte_controller_method(USSREnv *env, char *name, char *signature, ...);
+extern int64_t ussr_call_long_controller_method(USSREnv *env, char *name, char *signature, ...);
 extern jbyteArray ussr_charArray2byteArray(USSREnv *env, unsigned char *message, unsigned char messageSize);
 extern void ussr_byteArray2charArray(USSREnv *env, jbyteArray message, unsigned char messageSize, unsigned char *buffer);
 extern void ussr_releaseByteArray(USSREnv *env, jbyteArray array);
