0f623967910031f1d545d67a279ed069af42b9fb
mirko <mirko@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-08-29 01:21:02 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1051 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- native/base/atron_tinyos.c
+++ native/base/atron_tinyos.c
@@ -26,11 +26,43 @@ void printf_to_system_out_print(USSREnv *env, char* buf) {
   jstring str = ussr_charArray2string(env, buf);
   ussr_call_void_controller_method(env, "printfFromC", "(Ljava/lang/String;)V", str);
 }
+
+
+//this does not really work ... !
+void setPositionCentralJoint(USSREnv *env, int32_t position) {
+  ussr_call_void_controller_method(env, "setPositionCentralJoint", "(I)V", position);
+}
+
+
+void setSpeedCentralJoint(USSREnv *env, int32_t speed) {/*speed range: -100 100*/
+  ussr_call_void_controller_method(env, "setSpeedCentralJoint", "(I)V", speed);
+}
 int32_t sendMessage(USSREnv *env, uint8_t *message, int32_t messageSize, int32_t connector) {
   jbyteArray array = ussr_charArray2byteArray(env, message, messageSize);
   int32_t result = ussr_call_int_controller_method(env, "sendMessage", "([BII)I", array, messageSize, connector);
   ussr_releaseByteArray(env, array);
   return result;
+}
+int32_t getCentralJointEncoderValue(USSREnv *env) {
+  int result = ussr_call_int_controller_method(env, "getCentralJointEncoderValueInt", "()I");
+  return result;
+}
+uint8_t isConnected(USSREnv *env, uint8_t connector) {
+  int result = ussr_call_int_controller_method(env, "isConnected", "(I)Z", connector);
+  return result;
+}
+uint8_t isDisconnected(USSREnv *env, uint8_t connector) {
+  int result = ussr_call_int_controller_method(env, "isConnected", "(I)Z", connector);
+  if(result)
+    return 0;
+  else
+    return 1;
+}
+void connect(USSREnv *env, uint8_t connector) {
+  ussr_call_void_controller_method(env, "connect", "(I)V", connector);
+}
+void disconnect(USSREnv *env, uint8_t connector) {
+  ussr_call_void_controller_method(env, "disconnect", "(I)V", connector);
 }
 
 /* API upcalls/event */
