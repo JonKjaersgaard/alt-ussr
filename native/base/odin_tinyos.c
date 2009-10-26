7d4d480e9a84e0cb2a4bc13159b31edb8b8e275a
mirko <mirko@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-10-26 20:42:33 +0000

    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1256 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ native/base/odin_tinyos.c
@@ -0,0 +1,105 @@
+#include <odin_tinyos.h>
+#include <ussr_internal.h>
+
+#include <nativeController.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdint.h>
+
+#include "auto_generated_dispatcher_macros.h"
+
+void activate(USSRONLY(USSREnv *env)) {
+  int moduleId = ussr_call_int_controller_method(env, "getRole", "()I");/* the old "role" */
+
+__AUTO_GENERATED_DISPATCHER_MAIN__
+
+}
+
+int32_t initialize(USSRONLY(USSREnv *env)) {
+  return 0;/*initializationContext (could we use it to disambiguate controllers in lieu of the role?)*/
+}
+
+/* API downcalls/commands */
+void ussr_stub(USSREnv *env) {
+  ussr_call_void_controller_method(env, "stubCall", "()V");
+}
+int getRandomNumber(USSREnv *env) {
+  int random = ussr_call_int_controller_method(env, "getRandomNumber", "()I");
+  return random;
+}
+int moduleType(USSREnv *env) {
+  int type = ussr_call_int_controller_method(env, "moduleType", "()I");
+  return type;
+}
+/* //this does not really work ... ! */
+/* void setPositionCentralJoint(USSREnv *env, int32_t position) { */
+/*   ussr_call_void_controller_method(env, "setPositionCentralJoint", "(I)V", position); */
+/* } */
+
+
+/* void setSpeedCentralJoint(USSREnv *env, int32_t speed) {/\*speed range: -100 100*\/ */
+/*   ussr_call_void_controller_method(env, "setSpeedCentralJoint", "(I)V", speed); */
+/* } */
+int32_t sendMessage(USSREnv *env, uint8_t *message, int32_t messageSize, int32_t connector) {
+  jbyteArray array = ussr_charArray2byteArray(env, message, messageSize);
+  int32_t result = ussr_call_int_controller_method(env, "sendMessage", "([BII)I", array, messageSize, connector);
+  ussr_releaseByteArray(env, array);
+  return result;
+}
+
+/* API upcalls/event */
+void JNICALL Java_ussr_samples_odin_natives_OdinNativeTinyOSController_nativeSendDone(JNIEnv *jniENV, jobject self, jint initializationContext, /**/jint error, jint connector) {
+  int moduleId;
+  USSREnv env;
+  env.jnienv = jniENV;
+  env.controller = self;
+  env.context = initializationContext;
+
+  moduleId = ussr_call_int_controller_method(&env, "getRole", "()I");/* the old "role" */
+
+__AUTO_GENERATED_DISPATCHER_SENDDONE__
+
+}
+void JNICALL Java_ussr_samples_odin_natives_OdinNativeTinyOSController_nativeHandleMessage(JNIEnv *jniENV, jobject self, jint initializationContext, jbyteArray message, jint messageSize, jint channel) {
+  int moduleId;
+  unsigned char buffer[USSR_MAX_MESSAGE_SIZE];
+  USSREnv env;
+  env.jnienv = jniENV;
+  env.controller = self;
+  env.context = initializationContext;
+
+  moduleId = ussr_call_int_controller_method(&env, "getRole", "()I");/* the old "role" */
+
+  ussr_byteArray2charArray(&env, message, messageSize, buffer);
+
+__AUTO_GENERATED_DISPATCHER_HANDLEMESSAGE__
+
+}
+
+void JNICALL Java_ussr_samples_odin_natives_OdinNativeTinyOSController_nativeMillisecondElapsed(JNIEnv *jniENV, jobject self, jint initializationContext) {
+  int moduleId;
+  USSREnv env;
+  env.jnienv = jniENV;
+  env.controller = self;
+  env.context = initializationContext;
+
+  moduleId = ussr_call_int_controller_method(&env, "getRole", "()I");/* the old "role" */
+
+__AUTO_GENERATED_DISPATCHER_MILLISECONDELAPSED__
+
+}
+
+void JNICALL Java_ussr_samples_odin_natives_OdinNativeTinyOSController_native_1stub(JNIEnv *jniENV, jobject self, jint initializationContext) {
+  int moduleId;
+  USSREnv env;
+  env.jnienv = jniENV;
+  env.controller = self;
+  env.context = initializationContext;
+
+  moduleId = ussr_call_int_controller_method(&env, "getRole", "()I");/* the old "role" */
+
+__AUTO_GENERATED_DISPATCHER_NATIVESTUB__
+
+}
---
+++ native/include/odin_tinyos.h
@@ -0,0 +1,29 @@
+#ifndef ODIN_TINYOS_H_
+#define ODIN_TINYOS_H_
+
+#include <ussr.h>
+
+/*TODO: link it to TOSH_DATA_LENGTH*/
+#define USSR_MAX_MESSAGE_SIZE 128
+
+#ifdef USSR
+void ussr_stub();
+int getRandomNumber(USSREnv *env);
+int moduleType(USSREnv *env);
+int32_t sendMessage(USSRONLYC(USSREnv *env) uint8_t *message, int32_t messageSize, int32_t connector);
+#else
+
+#ifdef EMPTY_ODIN_API
+
+#include "odin.h"
+
+#else
+
+#include <api/API.h>
+
+#endif
+
+#endif
+
+
+#endif /*ODIN_TINYOS_H_*/
