e70b0d9bad1253b468bb213cc95c46894a962fc8
kokuz06 <kokuz06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-11-11 16:51:46 +0000

    30 day (Working on MainFrame). Transition to Remote simulation. Loading of simulation XML file from GUI started.
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1429 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/aGui/tabs/controllers/SimulationTabController.java
+++ src/ussr/aGui/tabs/controllers/SimulationTabController.java
@@ -1,12 +1,29 @@
 package ussr.aGui.tabs.controllers;
 
+import javax.swing.table.DefaultTableModel;
+
+import ussr.aGui.tabs.view.visualizer.ModuleCommunicationVisualizer;
+import ussr.aGui.tabs.views.SimulationTab;
+import ussr.builder.SimulationDescriptionConverter;
+import ussr.builder.enumerations.XMLTagsUsed;
+
 
 public class SimulationTabController {
 
 	
+	private static SimulationDescriptionConverter descriptionConverter;
 	
-	public void updateTable(){
-		
+	public static void setDescriptionConverter(SimulationDescriptionConverter descriptionConverter) {
+		SimulationTabController.descriptionConverter = descriptionConverter;
 	}
+
+	public static void updateTable(){
+		
+		javax.swing.table.DefaultTableModel model = (DefaultTableModel) SimulationTab.getJTable1().getModel();
+		//descriptionConverter.
+		model.addRow(new Object[]{"Plane size",descriptionConverter.convertPlaneSize()});
+	}
+	
+	
 
 }
--- src/ussr/aGui/tabs/views/SimulationTab.java
+++ src/ussr/aGui/tabs/views/SimulationTab.java
@@ -9,92 +9,69 @@ import ussr.aGui.tabs.Tabs;
 import ussr.physics.jme.JMESimulation;
 
 /**
-  
+
  * @author Konstantinas
  */
 public class SimulationTab extends Tabs {
 
-	
+
 	/**
 	 * The constrains of grid bag layout used during design of the tab.
 	 */
 	public  GridBagConstraints gridBagConstraints = new GridBagConstraints();
-	
+
 	public SimulationTab(boolean initiallyVisible, boolean firstTabbedPane, String tabTitle,JMESimulation jmeSimulation, String imageIconDirectory){
 		super(initiallyVisible,firstTabbedPane,tabTitle,jmeSimulation,imageIconDirectory);
-		
+
 		/*instantiate new panel, which will be the container for all components situated in the tab*/		
 		super.jComponent = new javax.swing.JPanel(new GridBagLayout());
 		initComponents();
 	}
 
 	/**
-     * Initializes the visual appearance of all components in the tab.
-     * Follows Strategy  pattern.
-     */
+	 * Initializes the visual appearance of all components in the tab.
+	 * Follows Strategy  pattern.
+	 */
 	public void initComponents() {
-		
-		
-			jScrollPane3 = new javax.swing.JScrollPane();
-			
-			
-	        jTable1 = new javax.swing.JTable();        
-	        
-		 
-	        
-	        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-	                new Object [][] {//rows
-	                  
-	                },
-	                new String [] {
-	                    "World Description", "Values", "PhysicsParameters", "Values"
-	                }
-	            ));
-	            jScrollPane3.setViewportView(jTable1);
-	            gridBagConstraints.fill = GridBagConstraints.PAGE_START;
-	    		gridBagConstraints.gridx = 0;
-	    		gridBagConstraints.gridy = 0;
-	    		super.jComponent.add(jScrollPane3,gridBagConstraints);	
 
-		 
-	/*        javax.swing.GroupLayout layout = new javax.swing.GroupLayout( super.jComponent);
-	        super.jComponent.setLayout(layout);
-	        layout.setHorizontalGroup(
-	            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-	            .addGroup(layout.createSequentialGroup()
-	                //.addContainerGap()
-	                //.addComponent(jLabel1)
-	                //.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-	                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-	                //.addContainerGap(304, Short.MAX_VALUE)
-	                )
-	        );
-	        layout.setVerticalGroup(
-	            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-	            .addGroup(layout.createSequentialGroup()
-	                //.addContainerGap()
-	                //.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-	                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-	                  //  .addComponent(jLabel1))
-	                //.addContainerGap(269, Short.MAX_VALUE)
-	                    )
-	        );*/
 
-	      
+		jScrollPane3 = new javax.swing.JScrollPane();
+
+
+		jTable1 = new javax.swing.JTable();        
+
+
+
+		jTable1.setModel(new javax.swing.table.DefaultTableModel(
+				new Object [][] {//rows
+
+				},
+				new String [] {
+						"World Description", "Value", "PhysicsParameters", "Value"
+				}
+		));
+		jScrollPane3.setViewportView(jTable1);
+		gridBagConstraints.fill = GridBagConstraints.PAGE_START;
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		super.jComponent.add(jScrollPane3,gridBagConstraints);	
+
+
+
 	}
-	
-	public javax.swing.JTable getJTable1() {
+
+	public static javax.swing.JTable getJTable1() {
 		return jTable1;
 	}
 
 	private javax.swing.JLabel jLabel1;
-    private javax.swing.JSpinner jSpinner1;
+	private javax.swing.JSpinner jSpinner1;
 
 	private javax.swing.JScrollPane jScrollPane3;
-	
-	
-	private javax.swing.JTable jTable1 ;
 
-	
+
+	private static javax.swing.JTable jTable1 ;
+
+
 
 }
