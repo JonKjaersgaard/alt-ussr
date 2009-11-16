c1e734e011a7dfbb1828b71bb2f877ec63d323c0
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-11-16 10:33:15 +0000

    enforce field holding id of main class
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1484 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/remote/facade/ParameterHolder.java
+++ src/ussr/remote/facade/ParameterHolder.java
@@ -3,7 +3,13 @@ package ussr.remote.facade;
 import java.io.Serializable;
 
 public abstract class ParameterHolder implements Serializable {
+
     private static ParameterHolder instance;
+    protected Class<?> mainClass;
+
+    public ParameterHolder(Class<?> mainClass) {
+        this.mainClass = mainClass;
+    }
     
     public static void set(ParameterHolder in) {
         instance = in;
@@ -11,5 +17,41 @@ public abstract class ParameterHolder implements Serializable {
     
     public static ParameterHolder get() {
         return instance;
+    }
+
+    public Class<?> getMainClass() {
+        return mainClass;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((mainClass == null) ? 0 : mainClass.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ParameterHolder other = (ParameterHolder) obj;
+        if (mainClass == null) {
+            if (other.mainClass != null)
+                return false;
+        } else if (!mainClass.equals(other.mainClass))
+            return false;
+        return true;
     }
 }
