cb1f0f42dff414264b57fbfef5af15042cc48b6c
kokuz06 <kokuz06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-11-17 08:26:48 +0000

    34 day (Working on MainFrame).  Transition to Remote simulation. A bit of refactoring.
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1507 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/remote/GUISimulationAdapter.java
+++ src/ussr/remote/GUISimulationAdapter.java
@@ -11,6 +11,7 @@ import ussr.aGui.GeneralController;
 import ussr.aGui.MainFrameSeparate;
 import ussr.aGui.MainFrameSeparateController;
 import ussr.aGui.MainFramesInter;
+import ussr.aGui.tabs.controllers.ConsoleTabController;
 import ussr.aGui.tabs.controllers.ConstructRobotTabController;
 import ussr.aGui.tabs.controllers.SimulationTabController;
 import ussr.aGui.tabs.controllers.TabsControllers;
@@ -20,6 +21,7 @@ import ussr.remote.facade.ActiveSimulation;
 import ussr.remote.facade.GUICallbackControlImpl;
 
 import ussr.remote.facade.RemotePhysicsSimulation;
+import ussr.samples.atron.simulations.ATRONRoleSimulation;
 import ussr.samples.atron.simulations.CommunicationDemo;
 
 /**
@@ -52,7 +54,7 @@ public class GUISimulationAdapter {
         // Start a simulation server process
         final ActiveSimulation simulation = server.launchSimulation();
         // Discard standard out (avoid buffers running full)
-        simulation.discardStandardOut();
+        //simulation.discardStandardOut();
         // Get standard err, pass it to method that prints it in separate thread
         dumpStream("err", simulation.getStandardErr());
         // Wait for simulation process to be ready to start a new simulation
@@ -66,9 +68,10 @@ public class GUISimulationAdapter {
                 try {
                     // Start using an xml file for a robot and a controller (both loaded by simulator process)
                    //simulation.start("samples/atron/car.xml", ussr.samples.atron.simulations.ATRONCarController1.class);
-                	//simulation.start(CommunicationDemo.class);
+                	simulation.start(ATRONRoleSimulation.class);
                 	//simulation.start("samples/atron/car.xml");
-                	simulation.start(simulationXMLFile);
+                	//simulation.start(simulationXMLFile);
+
                 } catch (RemoteException e) {
                     // Normal or abnormal termination, inspection of remote exception currently needed to determine...
                     System.err.println("Simulation stopped");
@@ -82,7 +85,6 @@ public class GUISimulationAdapter {
         while(sim==null) {
             System.out.println("Simulation still null");
             sim = simulation.getSimulation();
-           
             try {
                 Thread.sleep(1000);
             } catch(InterruptedException exn) {
@@ -101,6 +103,8 @@ public class GUISimulationAdapter {
         GeneralController.setBuilderController(sim.getBuilderControl());
         MainFrameSeparate.setMainFrameSeparateEnabled(true);
         SimulationTabController.updateTable();
+       
+        ConsoleTabController.setInputStream(simulation.getStandardOut());
         
 /*        while(true) {
             System.out.println(" remote simulation isPaused()="+sim.isPaused());
