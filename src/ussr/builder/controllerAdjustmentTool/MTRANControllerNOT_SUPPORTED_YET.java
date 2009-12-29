4f2d64c10f00de7735c0dd316c563e8811af5427
kokuz06 <kokuz06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2009-12-29 14:12:42 +0000

    57 day (Working on MainFrame). Changes in Tab AssignController. Adding editors
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1855 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/builder/controllerAdjustmentTool/MTRANControllerNOT_SUPPORTED_YET.java
+++ src/ussr/builder/controllerAdjustmentTool/MTRANControllerNOT_SUPPORTED_YET.java
@@ -1,16 +1,23 @@
 package ussr.builder.controllerAdjustmentTool;
 
 import ussr.model.Module;
+import ussr.samples.atron.ATRONController;
+import ussr.samples.mtran.MTRANController;
 
 
 public class MTRANControllerNOT_SUPPORTED_YET extends ControllerStrategy {
 
 	/**
+	 * The controller class providing ATRON API
+	 */
+	private MTRANController controller;	
+	
+	/**
 	 * The method which will is activated when this class is instantiated by the tool called "AssignControllerTool"
 	 * @param selectedModule, the module selected in simulation environment
 	 */
 	public void activate (Module selectedModule){		
-			//TODO
-		
+		controller = (MTRANController)selectedModule.getController();
+		controller.rotateContinuous(1, 1);
 	}	
 }
