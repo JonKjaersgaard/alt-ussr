a7b73f42b177a12cb0fa5e70f2ed638bff68f537
kokuz06 <kokuz06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2010-01-04 13:33:01 +0000

    61 day (Working on MainFrame). Experiments in visualizer tab and preparations to Demo.
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1871 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/builder/controllerAdjustmentTool/CKBOT_STANDARDControllerNOT_SUPPORTED_YET.java
+++ src/ussr/builder/controllerAdjustmentTool/CKBOT_STANDARDControllerNOT_SUPPORTED_YET.java
@@ -1,15 +1,24 @@
 package ussr.builder.controllerAdjustmentTool;
 
 import ussr.model.Module;
+import ussr.samples.atron.ATRONController;
+import ussr.samples.ckbot.CKBotController;
 
 
 public class CKBOT_STANDARDControllerNOT_SUPPORTED_YET extends ControllerStrategy {
 
+	
+	/**
+	 * The controller class providing the ATRON API
+	 */
+	private CKBotController controller;	
+	
 	/**
 	 * The method which will is activated when this class is instantiated by the tool called "AssignControllerTool"
 	 * @param selectedModule, the module selected in simulation environment
 	 */
 	public void activate (Module selectedModule){		
-		//TODO	
+		//controller = (CKBotController)selectedModule.getController();
+		//TODO
 	}	
 }
