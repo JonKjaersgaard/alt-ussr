28d29e7b92228ce11e0da61fd026ec592086692e
kokuz06 <kokuz06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2010-01-05 16:25:37 +0000

    62 day (Working on MainFrame). New(default) simulation button added and so on.
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1889 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ src/ussr/builder/simulationLoader/DefaultAtronController.java
@@ -0,0 +1,17 @@
+package ussr.builder.simulationLoader;
+
+import ussr.samples.atron.ATRONController;
+
+/**
+ * Just default controller which is used during loading default(new) simulation.
+ * @author Konstantinas
+ *
+ */
+public class DefaultAtronController extends ATRONController {
+
+	@Override
+	public void activate() {
+		// do nothing
+	}
+
+}
