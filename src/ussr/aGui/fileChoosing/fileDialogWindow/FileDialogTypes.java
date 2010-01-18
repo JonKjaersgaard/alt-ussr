e05c450e5bd986cf78d447347693c5408763c7f6
kokuz06 <kokuz06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2010-01-18 09:20:59 +0000

    72 day (Working on MainFrame).Working on compatibility issues of File chooser on Mac. Implementing FileDialog.
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@1972 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/aGui/fileChoosing/fileDialogWindow/FileDialogTypes.java
+++
@@ -1,9 +0,0 @@
-package ussr.aGui.fileChoosing.fileDialogWindow;
-
-public enum FileDialogTypes {
-OPEN_ROBOT_XML,
-OPEN_SIMULATION_XML,
-SAVE_ROBOT_XML,
-SAVE_SIMULATION_XML;
-
-}
--- src/ussr/aGui/fileChoosing/fileDialogWindow/controllers/FileDialogController.java
+++
@@ -1,8 +0,0 @@
-package ussr.aGui.fileChoosing.fileDialogWindow.controllers;
-
-import ussr.aGui.GeneralController;
-import ussr.aGui.fileChoosing.FileChooserControllerInter;
-
-public abstract class FileDialogController extends GeneralController implements FileDialogControllerInter {
-
-}
