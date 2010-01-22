a07b4bd291874bdc26dee7507691ec503dd9b102
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2010-01-22 14:36:45 +0000

    initial descriptions of most values
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@2025 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/aGui/enumerations/hintpanel/HintsSimulationTab.java
+++ src/ussr/aGui/enumerations/hintpanel/HintsSimulationTab.java
@@ -12,9 +12,12 @@ public enum HintsSimulationTab implements HintsTabsInter {
 			"to get familliar with the purpose of each element."),
 	SIMULATION("TODO "),
 	
-	PHYSICS_SIMULATION_STEP_SIZE("TODO"),
+	PHYSICS_SIMULATION_STEP_SIZE("The interval at which simulation time is discretized.  A higher value makes the simulation run faster, at the cost of precision.  " +
+			"Depending on the specific simulation, higher values can cause spurious effects (e.g., collision not detected for object with high velocity) or failures in the " +
+			"underlying physics system."),
 	
-	RESOLUTION_FACTOR("TODO"),
+	RESOLUTION_FACTOR("Factor that determines the number of polygons used to create geometrical primitives (sphere, cones, etc).  Decrease if the simulator uses all the available " +
+			"graphics memory (typically a simulation with many modules, see user guide).  Higher values gives less ugly graphics but has a performance overhead."),
 	
 	ROBOTS("TODO"),
 	
@@ -22,42 +25,45 @@ public enum HintsSimulationTab implements HintsTabsInter {
 	
 	PLANE_SIZE("The size of one edge of the underlying plane."),
 	
-	PLANE_TEXTURE("TODO"),
+	PLANE_TEXTURE("The texture used to decorate the underlying plane."),
 	
-	CAMERA_POSITION("TODO"),
+	CAMERA_POSITION("Overall preference for initial placement of the viewpoint on the simulation."),
 	
-	THE_WORLD_IS_FLAT("Whether is used a plane or a generated texture."),
+	THE_WORLD_IS_FLAT("Whether the underlying plane is flat or has a randomized terrain."),
 	
-	HAS_BACKGROUND_SCENERY("Whenever the background/overhead have clouds etc"),
+	HAS_BACKGROUND_SCENERY("Whether the background/overhead should have a landscape, clouds etc"),
 	
-	HAS_HEAVY_OBSTACLES("Whenever light or heavy obstacles are set in simulation environment."),
+	HAS_HEAVY_OBSTACLES("Cause default obstacles to be very heavy related to a normal module."),
 	
-	IS_FRAME_GRABBING_ACTIVE("TODO"),
+	IS_FRAME_GRABBING_ACTIVE("Grab frames from when the simulation starts."),
 	
 	PHYSICS_PARAMETERS("TODO"),
 	
-	DAMPING("TODO"),
+	DAMPING("Reduce all forces by a percentage at every physics step, causes less jumpy movements and gives less sensitivty to inertia."),
 	
-	REALISTIC_COLLISION("TODO"),
+	REALISTIC_COLLISION("When true, use the visible geometry to detect collision between bodies, otherwise use a more efficient approximation.  Realistic collision are normally " +
+			"required for self-reconfiguration and for certain locomotion experiments, but turn them off speeds up the simulation."),
 	
-	GRAVITY("TODO"),
+	GRAVITY("The gravity."),
 	
-	PLANE_MATERIAL("TODO"),
+	PLANE_MATERIAL("The material property of the surface of the underlying plane."),
 	
-	MAINTAIN_ROTATIONAL_JOINT_POSITIONS("TODO"),
+	MAINTAIN_ROTATIONAL_JOINT_POSITIONS("For robots that support this feature, cause actuators to actively maintain a position after they completed any movement."),
 	
-	ERROR_REDUCTION_PARAMETER("Error reduction parameter(ERP) controls the menchanism for alligning connected joints together. Joints misalignments can appear due to user specifying imprecise positions/rotations and during simulation, errors can creep so that, joints will drift away from each other." +
+	ERROR_REDUCTION_PARAMETER("Error reduction parameter(ERP) controls the menchanism for aligning connected joints together. Joints misalignments can appear due to user specifying imprecise positions/rotations and during simulation, errors can creep so that, joints will drift away from each other." +
 			                  "ERP specifies what proportion of joint error will be fixed during next simulation step. Recommended values are:0.1-0.8. Default is 0.8."),
 			                  
     CONSTRAINT_FORCE_MIXING("Constraint force mixing (CFM) is used to define constrains in mechanical interaction of objects. If it is set to zero, then objects will be as made of steel and will not penetrate each other in case of collision (connection)." +
     		                 "If CFM is set to positive value, then constraint force will be soft and object will exhibit softness in connectivity. Here typical use is simulation of soft materials. Setting CFM to negative value is not recommended, because this can lead " +
     		                 "to instability."),	
     		                 
-    USE_MODULE_EVENT_QUEUE ("TODO"),
+    USE_MODULE_EVENT_QUEUE ("Controls the concurrency model of the module.  When true, all events are stored in a queue that can be read by the module thread.  When false, " +
+    		"an event thread delivers all events (pausing delivery of other events while this event is being processed)."),
     
-    SYNCHRONIZE_WITH_CONTROLLERS("TODO"),
+    SYNCHRONIZE_WITH_CONTROLLERS("Enable strict time synchronization between the simulator and controllers running in native environments supporting this feature.  " +
+    		"Currently, TinyOS controllers support this feature."),
     
-    PHYSICS_SIMULATION_CONTROLLER_STEP_FACTOR("TODO"),
+    PHYSICS_SIMULATION_CONTROLLER_STEP_FACTOR("When using strict time synchronization, this is the time step to use in the native controller."),
 			     
 			                ;
 	//READ MORE HERE:http://opende.sourceforge.net/wiki/index.php/Manual_%28Concepts%29#Joint_error_and_the_error_reduction_parameter_.28ERP.29
