639614f41204f4eec7ce50efa0ec42e09e28f76e
kokuz06 <kokuz06@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2010-01-27 13:54:04 +0000

    79 day (Working on MainFrame). Refactoring + documentation.
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@2061 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- src/ussr/remote/GUIRemoteSimulationAdapter.java
+++ src/ussr/remote/GUIRemoteSimulationAdapter.java
@@ -1,12 +1,10 @@
 package ussr.remote;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.rmi.RemoteException;
-
 
 import ussr.aGui.MainFrameSeparate;
 import ussr.aGui.MainFrames;
@@ -14,7 +12,7 @@ import ussr.aGui.controllers.GeneralController;
 import ussr.aGui.controllers.MainFrameSeparateController;
 import ussr.aGui.tabs.constructionTabs.ConstructRobotTab;
 import ussr.aGui.tabs.controllers.ConsoleTabController;
-import ussr.aGui.tabs.controllers.ConstructRobotTabController;
+
 import ussr.aGui.tabs.controllers.SimulationTabController;
 import ussr.aGui.tabs.simulation.SimulationTab;
 import ussr.aGui.tabs.simulation.SimulationTreeEditors;
@@ -32,7 +30,6 @@ import ussr.remote.facade.RemotePhysicsSimulation;
 public class GUIRemoteSimulationAdapter {
 
 	public static final int SERVER_PORT = 54323;
-	
 
 	/**
 	 * The server to run simulation on.
@@ -47,13 +44,6 @@ public class GUIRemoteSimulationAdapter {
 			throw new Error("Unable to create server: "+e);
 		}      
 	} 
-	
-/*	private static RemoteSimulationThread threadRemoteSimulation;
-	
-	public static RemoteSimulationThread getThreadRemoteSimulation() {
-		return threadRemoteSimulation;
-	}*/
-
 
 	/**
 	 * Starts simulation from specified xml file.
@@ -68,17 +58,17 @@ public class GUIRemoteSimulationAdapter {
 		} catch (IOException e) {
 			throw new Error("Unable to start simulation subprocess: "+e);
 		}
-		
+
 		// Get standard output and error streams and direct them to GUI console tab, so that the buffer is not full, which will cause simulation to stop.
 		ConsoleTabController.appendStreamToConsole("StandardOut", simulation.getStandardOut());
 		ConsoleTabController.appendStreamToConsole("Error/Info/Warning", simulation.getStandardErr());
-		
-		
-		// FIXME USED TEMPORARY
+
+
+		// FIXME USE THIS IF YOU ARE INTERESTED IN GETTING OUTPUT IN ECLIPSE CONSOLE
 		// Discard standard out (avoid buffers running full)
-        simulation.discardStandardOut();
-        // Get standard err, pass it to method that prints it in separate thread
-        dumpStream("err", simulation.getStandardErr());
+		// simulation.discardStandardOut();
+		// Get standard err, pass it to method that prints it in separate thread
+		//dumpStream("err", simulation.getStandardErr());
 
 
 		// Wait for simulation process to be ready to start a new simulation
@@ -86,45 +76,22 @@ public class GUIRemoteSimulationAdapter {
 			System.out.println("Waiting for simulation");
 			simulation.waitForReady();
 		}
-		
+
 		/*Set new position of simulation window relative to GUI*/
 		simulation.setWindowPosition(MainFrameSeparate.simWindowX, MainFrameSeparate.simWindowY);
-	
-		
-		// Start a simulation in the remote process
-		//threadRemoteSimulation = new RemoteSimulationThread(simulation,simulationXMLFile);
-		//threadRemoteSimulation.start();
-		
-	
-		
-		new Thread() {
-			
+
+		new Thread() {		
 			public void run() {
-			
 				try {
-					// Start using an xml file for a robot and a controller (both loaded by simulator process)
-					//simulation.start("samples/atron/car.xml", ussr.samples.atron.simulations.ATRONCarController1.class);
-					//simulation.start(SnakeCarDemo.class);
 					simulation.start(simulationXMLFile);
-					//ATRONSimulation1.class,ATRONCarSimulation
-					//NO ATRONRoleSimulation.class(broken), CommunicationDemo(null),ATRONTestSimulation.class(null),
-					//ConveyorSimulation(null),CrawlerSimulation(null),EightToCarSimulation(broken),SnakeCarDemo.class(null);
 				} catch (RemoteException e) {
-			
-					
 					// Normal or abnormal termination, inspection of remote exception currently needed to determine...
 					System.err.println("Simulation stopped");
 				}
-		}
-			
+			}
+
 		}.start();
-		//threadRemoteSimulation.start();
-		
-		
-		// Obtain a reference to remote PhysicsSimulation object (must wait for it to be instantiated remotely)
-	/*	if (threadRemoteSimulation.isUbNormalTemination()){
-			
-		}else{*/
+
 		RemotePhysicsSimulation sim = null;
 		while(sim==null) {
 			System.out.println("Simulation still null");
@@ -135,58 +102,57 @@ public class GUIRemoteSimulationAdapter {
 				throw new Error("Unexpected interruption");
 			}
 		}
-	
+
 		callBackGUI(simulation,sim);
-		//}
 	}
-	
+
 
 	/**
 	 * Sets remote objects of simulation in GUI and adapts it to simulation.
-	 * @param simulation
-	 * @param remotePhysicsSimulation
+	 * @param simulation, proxy for a simulation running in a different process. 
+	 * @param remotePhysicsSimulation, remote version of the standard PhysicsSimulation interface.
 	 */
 	private static void callBackGUI(ActiveSimulation simulation,RemotePhysicsSimulation remotePhysicsSimulation)throws IOException {
 		remotePhysicsSimulation.setGUICallbackControl(new GUICallbackControlImpl());
 		GeneralController.setRemotePhysicsSimulation(remotePhysicsSimulation);
 		MainFrameSeparateController.setRendererControl(remotePhysicsSimulation.getRendererControl());
-		
+
 		GeneralController.setBuilderControl(remotePhysicsSimulation.getBuilderControl());		
 		MainFrameSeparate.setMainFrameSeparateEnabled(true);		
-		 
-         SimulationSpecification simulationSpecification = simulation.getXmlSimulationProvider().getSimulationSpecification();
-         SimulationTabController.setSimulationSpecification(simulationSpecification);
-         SimulationTab.addRobotNodes(simulationSpecification,true,true);
-         MainFrames.deSelectConstructRobotButton();
-         
-         SimulationTreeEditors.updateValuesEditors();
-         SimulationTab.resizeComponents();
-         SimulationTab.setTabVisible(true); 
-         ConstructRobotTab.setTabEnabled(true);
+
+		SimulationSpecification simulationSpecification = simulation.getXmlSimulationProvider().getSimulationSpecification();
+		SimulationTabController.setSimulationSpecification(simulationSpecification);
+		SimulationTab.addRobotNodes(simulationSpecification,true,true);
+		MainFrames.deSelectConstructRobotButton();
+
+		SimulationTreeEditors.updateValuesEditors();
+		SimulationTab.resizeComponents();
+		SimulationTab.setTabVisible(true); 
+		ConstructRobotTab.setTabEnabled(true);
 	}
-	
-	// FIXME USED TEMPORARY
-	 /**
-     * Dump an input stream to standard out, prefixing all lines with a fixed text 
-     * @param prefix the prefix to use
-     * @param stream the stream to dump
-     */
-    private static void dumpStream(final String prefix, final InputStream stream) {
-        new Thread() {
-            public void run() {
-                BufferedReader input = new BufferedReader(new InputStreamReader(stream));
-                while(true) {
-                    String line;
-                    try {
-                        line = input.readLine();
-                        if(line==null) break;
-                        System.out.println(prefix+": "+line);
-                    } catch (IOException e) {
-                        throw new Error("Unable to dump stream: "+e); 
-                    }
-                }
-            }
-        }.start();
-    }    
+
+	/**
+	 * Dump an input stream to standard out, prefixing all lines with a fixed text 
+	 * @param prefix the prefix to use
+	 * @param stream the stream to dump
+	 *  FIXME IS USED WHEN OUTPUTS ARE DIRECTED TO ECLIPSE CONSOLE.
+	 */
+	private static void dumpStream(final String prefix, final InputStream stream) {
+		new Thread() {
+			public void run() {
+				BufferedReader input = new BufferedReader(new InputStreamReader(stream));
+				while(true) {
+					String line;
+					try {
+						line = input.readLine();
+						if(line==null) break;
+						System.out.println(prefix+": "+line);
+					} catch (IOException e) {
+						throw new Error("Unable to dump stream: "+e); 
+					}
+				}
+			}
+		}.start();
+	}    
 }
 
