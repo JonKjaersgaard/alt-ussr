d3a77b4a5172e4755277859fcbfb600fbc4f094b
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2010-06-09 13:48:09 +0000

    moving things around
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@2403 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

---
+++ contrib/g2/carsnakeSimpleGen_par.java
@@ -0,0 +1,414 @@
+/* DynaRole Java code generated for carsnakeSimple */
+/* DynaRole Java statemachine generated for carsnakeSimple */
+package g2;
+
+import rar.*;
+
+public class carsnakeSimpleGen_par extends StateMachine {
+
+  private static final int CENTER_ERROR_TOLERANCE = 5;
+  private int token = 255;
+  private int myID=-1;
+  private boolean isDone = false; 
+  private static final boolean TRUE = true;
+  private static final boolean FALSE = false;
+  
+  private boolean doneRotatingTo(int goal) { 
+	
+	if(api.isRotating()) {
+		return false; 
+	}
+
+	return true;
+  }
+
+  private void rotateDirTo(int to, boolean direction) {
+	api.rotateDirToInDegrees(to, direction);
+}
+
+
+  private void connect(int connector) {
+	api.connect(connector);
+  }
+
+  private void disconnect(int connector) {
+	api.disconnect(connector);
+  }
+
+  private boolean notDoneConnecting(int connector) {
+	return !api.isConnected(connector);
+} 
+ 
+  private boolean notDoneDisconnecting(int connector) {
+	return api.isConnected(connector);
+  }
+
+  protected void stateMachine() { 
+    api.yield();
+    if(token == 255) { /* try to see if there's a new state for me */
+      token = stateManager.getMyNewState();
+	  if(token!=255) {
+		System.out.println(myID+": Now performing state "+token);
+	  }
+    }
+	
+	switch(token) {
+	case 0: token = 1; /* fall-through */
+    case 1: /* Module M__0 */
+      stateManager.addPendingState(1);
+      disconnect(0);
+      token = 2;
+      break;
+    case 2:
+      stateManager.sendState(4,3);
+      token = 3; /* fall-through */
+    case 3:
+      if(notDoneDisconnecting(0)) break;
+      stateManager.removePendingState(1);
+      token = 255;
+      break;
+    case 4: /* Module M__3 */
+      disconnect(4);
+      token = 5;
+      break;
+    case 5:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(4)) break;
+      token = 6; /* fall-through */
+    case 6: /* Module M__3 */
+      rotateDirTo(324,FALSE);
+      token = 7;
+      break;
+    case 7:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(324)) break;
+      stateManager.sendState(8,4);
+      token = 255;
+      break;
+    case 8: /* Module M__4 */
+      rotateDirTo(108,TRUE);
+      token = 9;
+      break;
+    case 9:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(108)) break;
+      token = 10; /* fall-through */
+    case 10: /* Module M__4 */
+      stateManager.addPendingState(10);
+      connect(0);
+      token = 11;
+      break;
+    case 11:
+      stateManager.sendState(13,1);
+      token = 12; /* fall-through */
+    case 12:
+      if(notDoneConnecting(0)) break;
+      stateManager.removePendingState(10);
+      token = 255;
+      break;
+    case 13: /* Module M__1 */
+      stateManager.addPendingState(13);
+      rotateDirTo(324,FALSE);
+      token = 14;
+      break;
+    case 14:
+      stateManager.sendState(16,6);
+      token = 15; /* fall-through */
+    case 15:
+      if(!doneRotatingTo(324)) break;
+      stateManager.removePendingState(13);
+      token = 255;
+      break;
+    case 16: /* Module M__6 */
+      disconnect(2);
+      token = 17;
+      break;
+    case 17:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(2)) break;
+      stateManager.sendState(18,4);
+      token = 255;
+      break;
+    case 18: /* Module M__4 */
+      stateManager.addPendingState(18);
+      rotateDirTo(216,TRUE);
+      token = 19;
+      break;
+    case 19:
+      stateManager.sendState(21,6);
+      token = 20; /* fall-through */
+    case 20:
+      if(!doneRotatingTo(216)) break;
+      stateManager.removePendingState(18);
+      token = 255;
+      break;
+    case 21: /* Module M__6 */
+      rotateDirTo(108,TRUE);
+      token = 22;
+      break;
+    case 22:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(108)) break;
+      stateManager.sendState(23,0);
+      token = 255;
+      break;
+    case 23: /* Module M__0 */
+      connect(0);
+      token = 24;
+      break;
+    case 24:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(0)) break;
+      stateManager.sendState(25,6);
+      token = 255;
+      break;
+    case 25: /* Module M__6 */
+      disconnect(6);
+      token = 26;
+      break;
+    case 26:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(6)) break;
+      stateManager.sendState(27,0);
+      token = 255;
+      break;
+    case 27: /* Module M__0 */
+      stateManager.addPendingState(27);
+      rotateDirTo(324,FALSE);
+      token = 28;
+      break;
+    case 28:
+      stateManager.sendState(30,1);
+      token = 29; /* fall-through */
+    case 29:
+      if(!doneRotatingTo(324)) break;
+      stateManager.removePendingState(27);
+      token = 255;
+      break;
+    case 30: /* Module M__1 */
+      rotateDirTo(0,TRUE);
+      token = 31;
+      break;
+    case 31:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(0)) break;
+      stateManager.sendState(32,0);
+      token = 255;
+      break;
+    case 32: /* Module M__0 */
+      rotateDirTo(0,TRUE);
+      token = 33;
+      break;
+    case 33:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(0)) break;
+      stateManager.sendState(34,5);
+      token = 255;
+      break;
+    case 34: /* Module M__5 */
+      stateManager.addPendingState(34);
+      connect(0);
+      token = 35;
+      break;
+    case 35:
+      stateManager.sendState(37,2);
+      token = 36; /* fall-through */
+    case 36:
+      if(notDoneConnecting(0)) break;
+      stateManager.removePendingState(34);
+      token = 255;
+      break;
+    case 37: /* Module M__2 */
+      stateManager.addPendingState(37);
+      connect(4);
+      token = 38;
+      break;
+    case 38:
+      stateManager.sendState(40,1);
+      token = 39; /* fall-through */
+    case 39:
+      if(notDoneConnecting(4)) break;
+      stateManager.removePendingState(37);
+      token = 255;
+      break;
+    case 40: /* Module M__1 */
+      connect(4);
+      token = 41;
+      break;
+    case 41:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(4)) break;
+      stateManager.sendState(42,4);
+      token = 255;
+      break;
+    case 42: /* Module M__4 */
+      disconnect(0);
+      token = 43;
+      break;
+    case 43:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(0)) break;
+      stateManager.sendState(44,3);
+      token = 255;
+      break;
+    case 44: /* Module M__3 */
+      disconnect(6);
+      token = 45;
+      break;
+    case 45:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(6)) break;
+      stateManager.sendState(46,1);
+      token = 255;
+      break;
+    case 46: /* Module M__1 */
+      stateManager.addPendingState(46);
+      rotateDirTo(108,TRUE);
+      token = 47;
+      break;
+    case 47:
+      stateManager.sendState(49,3);
+      token = 48; /* fall-through */
+    case 48:
+      if(!doneRotatingTo(108)) break;
+      stateManager.removePendingState(46);
+      token = 255;
+      break;
+    case 49: /* Module M__3 */
+      rotateDirTo(0,TRUE);
+      token = 50;
+      break;
+    case 50:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(0)) break;
+      stateManager.sendState(51,1);
+      token = 255;
+      break;
+    case 51: /* Module M__1 */
+      connect(6);
+      token = 52;
+      break;
+    case 52:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(6)) break;
+      stateManager.sendState(53,3);
+      token = 255;
+      break;
+    case 53: /* Module M__3 */
+      /* { */
+        disconnect(0);
+        disconnect(2);
+      /* } */
+      token = 54;
+      break;
+    case 54:
+        // local blocking handlers:
+        if(notDoneDisconnecting(0)) break;
+        if(notDoneDisconnecting(2)) break;
+      stateManager.sendState(55,1);
+      token = 255;
+      break;
+    case 55: /* Module M__1 */
+      rotateDirTo(216,TRUE);
+      token = 56;
+      break;
+    case 56:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(216)) break;
+
+	
+	stateManager.sendState(254,-1);
+	token=255;
+	break;
+	
+	case 255:
+		if(stateManager.getGlobalState()==254) {
+			if(!isDone) {
+				System.out.println(myID+": i am done...");
+				api.reportResult(true);
+			}
+			isDone = true;
+		}
+		break;
+	}
+}
+
+  public void init(int self_id) {
+    int address;
+    if((self_id==0)) address = 0;
+    else if((self_id==1)) address = 1;
+    else if((self_id==2)) address = 2;
+    else if((self_id==3)) address = 3;
+    else if((self_id==4)) address = 4;
+    else if((self_id==5)) address = 5;
+    else if((self_id==6)) address = 6;
+
+    else address = 127;
+	myID = address;
+	token = 255;
+	
+    api.setLeds(myID);
+	reset_state();
+    stateManager.init(myID);
+	isDone = false;
+}
+
+  public void reset_sequence() {
+    stateManager.reset_sequence();
+}
+
+  public void reset_state() {
+    stateManager.reset_state();
+}
+
+  public boolean checkPendingStateResponsibility(int address, int pendingState) {
+    if(address==5) {
+      if(pendingState==34) return true;
+      return false;
+    }
+    if(address==0) {
+      if(pendingState==1) return true;
+      if(pendingState==27) return true;
+      return false;
+    }
+    if(address==4) {
+      if(pendingState==18) return true;
+      if(pendingState==10) return true;
+      return false;
+    }
+    if(address==1) {
+      if(pendingState==46) return true;
+      if(pendingState==13) return true;
+      return false;
+    }
+    if(address==2) {
+      if(pendingState==37) return true;
+      return false;
+    }
+    return false;
+  }
+
+  int getLastState(int address) {
+    if(address==0) return 32;
+    if(address==1) return 55;
+    if(address==2) return 37;
+    if(address==3) return 53;
+    if(address==4) return 42;
+    if(address==5) return 34;
+    if(address==6) return 25;
+    return 255;
+  }
+  
+  int getLastStateLowerBound(int address) {
+    if(address==0) return 4;
+    if(address==1) return 25;
+    if(address==2) return 4;
+    if(address==3) return 25;
+    if(address==4) return 4;
+    if(address==5) return 4;
+    if(address==6) return 4;
+    return 255;
+  }
+
+
+}
---
+++ contrib/g2/carsnakeSimpleGen_seq.java
@@ -0,0 +1,381 @@
+/* DynaRole Java code generated for carsnakeSimple */
+/* DynaRole Java statemachine generated for carsnakeSimple */
+package g2;
+
+import rar.*;
+
+public class carsnakeSimpleGen_seq extends StateMachine {
+
+  private static final int CENTER_ERROR_TOLERANCE = 5;
+  private int token = 255;
+  private int myID=-1;
+  private boolean isDone = false; 
+  private static final boolean TRUE = true;
+  private static final boolean FALSE = false;
+  
+  private boolean doneRotatingTo(int goal) { 
+	
+	if(api.isRotating()) {
+		return false; 
+	}
+
+	return true;
+  }
+
+  private void rotateDirTo(int to, boolean direction) {
+	api.rotateDirToInDegrees(to, direction);
+}
+
+
+  private void connect(int connector) {
+	api.connect(connector);
+  }
+
+  private void disconnect(int connector) {
+      if(myID==0 && connector==0) System.out.println("FOO!!!");
+      if(myID==6 && connector==3) System.out.println("BAR!!!");
+	api.disconnect(connector);
+  }
+
+  private boolean notDoneConnecting(int connector) {
+	return !api.isConnected(connector);
+} 
+ 
+  private boolean notDoneDisconnecting(int connector) {
+	return api.isConnected(connector);
+  }
+
+  protected void stateMachine() { 
+    api.yield();
+    //updateLocalState();
+    if(token == 255) { /* try to see if there's a new state for me */
+      token = stateManager.getMyNewState();
+	  if(token!=255) {
+		System.out.println(myID+": Now performing state "+token/*+"@"+stateManager.getTime()*/);
+		//System.out.println("Global state: "+getGlobalState());
+	  }
+    }
+	
+	switch(token) {
+	case 0: token = 1; /* fall-through */
+    case 1: /* Module M__0 */
+      disconnect(0);
+      token = 2;
+      break;
+    case 2:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(0)) break;
+      stateManager.sendState(3,3);
+      token = 255;
+      break;
+    case 3: /* Module M__3 */
+      disconnect(4);
+      token = 4;
+      break;
+    case 4:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(4)) break;
+      token = 5; /* fall-through */
+    case 5: /* Module M__3 */
+      rotateDirTo(324,FALSE);
+      token = 6;
+      break;
+    case 6:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(324)) break;
+      stateManager.sendState(7,4);
+      token = 255;
+      break;
+    case 7: /* Module M__4 */
+      rotateDirTo(108,TRUE);
+      token = 8;
+      break;
+    case 8:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(108)) break;
+      token = 9; /* fall-through */
+    case 9: /* Module M__4 */
+      connect(0);
+      token = 10;
+      break;
+    case 10:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(0)) break;
+      stateManager.sendState(11,1);
+      token = 255;
+      break;
+    case 11: /* Module M__1 */
+      rotateDirTo(324,FALSE);
+      token = 12;
+      break;
+    case 12:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(324)) break;
+      stateManager.sendState(13,6);
+      token = 255;
+      break;
+    case 13: /* Module M__6 */
+      disconnect(2);
+      token = 14;
+      break;
+    case 14:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(2)) break;
+      stateManager.sendState(15,4);
+      token = 255;
+      break;
+    case 15: /* Module M__4 */
+      rotateDirTo(216,TRUE);
+      token = 16;
+      break;
+    case 16:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(216)) break;
+      stateManager.sendState(17,6);
+      token = 255;
+      break;
+    case 17: /* Module M__6 */
+      rotateDirTo(108,TRUE);
+      token = 18;
+      break;
+    case 18:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(108)) break;
+      stateManager.sendState(19,0);
+      token = 255;
+      break;
+    case 19: /* Module M__0 */
+      connect(0);
+      token = 20;
+      break;
+    case 20:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(0)) break;
+      stateManager.sendState(21,6);
+      token = 255;
+      break;
+    case 21: /* Module M__6 */
+      disconnect(6);
+      token = 22;
+      break;
+    case 22:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(6)) break;
+      stateManager.sendState(23,0);
+      token = 255;
+      break;
+    case 23: /* Module M__0 */
+      rotateDirTo(324,FALSE);
+      token = 24;
+      break;
+    case 24:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(324)) break;
+      stateManager.sendState(25,1);
+      token = 255;
+      break;
+    case 25: /* Module M__1 */
+      rotateDirTo(0,TRUE);
+      token = 26;
+      break;
+    case 26:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(0)) break;
+      stateManager.sendState(27,0);
+      token = 255;
+      break;
+    case 27: /* Module M__0 */
+      rotateDirTo(0,TRUE);
+      token = 28;
+      break;
+    case 28:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(0)) break;
+      stateManager.sendState(29,5);
+      token = 255;
+      break;
+    case 29: /* Module M__5 */
+      connect(0);
+      token = 30;
+      break;
+    case 30:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(0)) break;
+      stateManager.sendState(31,2);
+      token = 255;
+      break;
+    case 31: /* Module M__2 */
+      connect(4);
+      token = 32;
+      break;
+    case 32:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(4)) break;
+      stateManager.sendState(33,1);
+      token = 255;
+      break;
+    case 33: /* Module M__1 */
+      connect(4);
+      token = 34;
+      break;
+    case 34:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(4)) break;
+      stateManager.sendState(35,4);
+      token = 255;
+      break;
+    case 35: /* Module M__4 */
+      disconnect(0);
+      token = 36;
+      break;
+    case 36:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(0)) break;
+      stateManager.sendState(37,3);
+      token = 255;
+      break;
+    case 37: /* Module M__3 */
+      disconnect(6);
+      token = 38;
+      break;
+    case 38:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(6)) break;
+      stateManager.sendState(39,1);
+      token = 255;
+      break;
+    case 39: /* Module M__1 */
+      rotateDirTo(108,TRUE);
+      token = 40;
+      break;
+    case 40:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(108)) break;
+      stateManager.sendState(41,3);
+      token = 255;
+      break;
+    case 41: /* Module M__3 */
+      rotateDirTo(0,TRUE);
+      token = 42;
+      break;
+    case 42:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(0)) break;
+      stateManager.sendState(43,1);
+      token = 255;
+      break;
+    case 43: /* Module M__1 */
+      connect(6);
+      token = 44;
+      break;
+    case 44:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneConnecting(6)) break;
+      stateManager.sendState(45,3);
+      token = 255;
+      break;
+    case 45: /* Module M__3 */
+      disconnect(0);
+      token = 46;
+      break;
+    case 46:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(0)) break;
+      token = 47; /* fall-through */
+    case 47: /* Module M__3 */
+      disconnect(2);
+      token = 48;
+      break;
+    case 48:
+      if(stateManager.pendingStatesPresent()) break;
+      if(notDoneDisconnecting(2)) break;
+      stateManager.sendState(49,1);
+      token = 255;
+      break;
+    case 49: /* Module M__1 */
+      rotateDirTo(216,TRUE);
+      token = 50;
+      break;
+    case 50:
+      if(stateManager.pendingStatesPresent()) break;
+      if(!doneRotatingTo(216)) break;
+
+	
+	stateManager.sendState(254,-1);
+	token=255;
+	break;
+	
+	case 255:
+		if(stateManager.getGlobalState()==254) {
+			if(!isDone) {
+				System.out.println(myID+": i am done...");
+				api.reportResult(true);
+			}
+			isDone = true;
+		}
+		break;
+	}
+}
+
+  public void init(int self_id) {
+    int address;
+    if((self_id==0)) address = 0;
+    else if((self_id==1)) address = 1;
+    else if((self_id==2)) address = 2;
+    else if((self_id==3)) address = 3;
+    else if((self_id==4)) address = 4;
+    else if((self_id==5)) address = 5;
+    else if((self_id==6)) address = 6;
+
+    else address = 127;
+	myID = address;
+	token = 255;
+	
+    api.setLeds(myID);
+	reset_state();
+    stateManager.init(myID);
+	isDone = false;
+}
+
+  public void reset_sequence() {
+    //if ( myID == 0 ) {
+      //token = 0; // Tolerate reset in the middle of reconfiguration
+    //}    
+    stateManager.reset_sequence();
+}
+
+  public void reset_state() {
+    //if ( myID == 0 ) {
+    //  token = 0; // Tolerate reset in the middle of reconfiguration
+    //}    
+    stateManager.reset_state();
+}
+
+  public boolean checkPendingStateResponsibility(int address, int pendingState) {
+    return false;
+  }
+
+  int getLastState(int address) {
+    if(address==0) return 27;
+    if(address==1) return 49;
+    if(address==2) return 31;
+    if(address==3) return 45;
+    if(address==4) return 35;
+    if(address==5) return 29;
+    if(address==6) return 21;
+    return 255;
+  }
+  
+  int getLastStateLowerBound(int address) {
+    if(address==0) return 3;
+    if(address==1) return 21;
+    if(address==2) return 3;
+    if(address==3) return 21;
+    if(address==4) return 3;
+    if(address==5) return 3;
+    if(address==6) return 3;
+    return 255;
+  }
+
+
+}
