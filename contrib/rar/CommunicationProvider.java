14fa6896e432e23594d338428b1d628604b1bc41
ups <ups@6f8a1012-4fc2-4a14-a60e-34c1cfb229e2>
2010-06-10 08:02:16 +0000

    package renaming
    git-svn-id: https://svn.mip.sdu.dk/mrl-public/ussr@2423 6f8a1012-4fc2-4a14-a60e-34c1cfb229e2

--- contrib/rar/CommunicationProvider.java
+++
@@ -1,8 +0,0 @@
-package rar;
-
-public interface CommunicationProvider {
-    public void broadcastMessage(byte[] message);
-    public void delay(int time);
-    public float getTime();
-    public boolean isResponsible(int myID, int i);
-}
